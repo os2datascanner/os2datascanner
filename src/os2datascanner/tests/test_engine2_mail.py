70c0eeb1617a57d3c23107b616adf490f362e128
Alexander Faithfull <af@magenta.dk>
2020-05-28 13:43:52 +0200

    [#36462] Added a simple test for .eml file traversal
    This fails at present because MailSource makes unsound assumptions about
    its backing Handle. Let's fix that, eh?

---
+++ src/os2datascanner/tests/test_engine2_mail.py
@@ -0,0 +1,29 @@
+import os.path
+from unittest import TestCase
+
+from os2datascanner.engine2.model.core import Source, SourceManager
+from os2datascanner.engine2.model.file import (
+        FilesystemHandle, FilesystemSource)
+from os2datascanner.engine2.model.derived.mail import (
+        MailSource, MailPartHandle)
+
+
+here_path = os.path.dirname(__file__)
+test_data_path = os.path.join(here_path, "data", "mail")
+
+
+
+class Engine2MailTest(TestCase):
+    def test_eml_files(self):
+        fs = FilesystemSource(test_data_path)
+        with SourceManager() as sm:
+            for h in fs.handles(sm):
+                mail_source = Source.from_handle(h)
+                self.assertIsInstance(
+                        mail_source,
+                        MailSource,
+                        "conversion of {0} to MailSource failed".format(h))
+                for h in mail_source.handles(sm):
+                    self.assertIsInstance(
+                            h,
+                            MailPartHandle)
