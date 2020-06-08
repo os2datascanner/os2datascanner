5bed736fe3612c7db3111ed36cc20fb29f6cb0a8
Alexander Faithfull <af@magenta.dk>
2020-06-08 13:15:34 +0200

    [#34154] Added NamedTuple definitions of all of the pipeline messages
    At the moment, MatchesMessage is the only one of these that the rest of the
    code actually uses. Let's fix that

--- src/os2datascanner/engine2/pipeline/messages.py
+++ src/os2datascanner/engine2/pipeline/messages.py
@@ -1,14 +1,108 @@
-from typing import NamedTuple
+from typing import Sequence, NamedTuple
 
-from ..model.core import Handle
-from ..rules.rule import Sensitivity
+from ..model.core import Handle, Source
+from ..rules.rule import Rule, SimpleRule, Sensitivity
+
+
+class MatchFragment(NamedTuple):
+    rule: SimpleRule
+    matches: Sequence[dict]
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return MatchFragment(
+                rule=Rule.from_json_object(obj["rule"]),
+                matches=obj["matches"])
+
+
+class ProgressFragment(NamedTuple):
+    rule: Rule
+    matches: Sequence[MatchFragment]
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return ProgressFragment(
+                rule=Rule.from_json_object(obj["rule"]),
+                matches=[MatchFragment.from_json_object(mf)
+                        for mf in obj["matches"]])
+
+
+class ScanSpecMessage(NamedTuple):
+    scan_tag: object
+    source: Source
+    rule: Rule
+    configuration: dict
+    progress: ProgressFragment
+
+    @classmethod
+    def from_json_object(cls, obj):
+        progress_fragment = obj.get("progress")
+        return ScanSpecMessage(
+                scan_tag=obj["scan_tag"],
+                source=Source.from_json_object(obj["source"]),
+                rule=Rule.from_json_object(obj["rule"]),
+                configuration=obj.get("configuration"),
+                progress=ProgressFragment.from_json_object(progress_fragment)
+                        if progress_fragment else None)
+
+
+class ConversionMessage(NamedTuple):
+    scan_spec: ScanSpecMessage
+    handle: Handle
+    progress: ProgressFragment
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return ConversionMessage(
+                scan_spec=ScanSpecMessage.from_json_object(obj["scan_spec"]),
+                handle=Handle.from_json_object(obj["handle"]),
+                progress=ProgressFragment.from_json_object(obj["progress"]))
+
+
+class RepresentationMessage(NamedTuple):
+    scan_spec: ScanSpecMessage
+    handle: Handle
+    progress: ProgressFragment
+    representations: dict
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return RepresentationMessage(
+                scan_spec=ScanSpecMessage.from_json_object(obj["scan_spec"]),
+                handle=Handle.from_json_object(obj["handle"]),
+                progress=ProgressFragment.from_json_object(obj["progress"]),
+                representations=obj["representations"])
+
+
+class HandleMessage(NamedTuple):
+    scan_tag: object
+    handle: Handle
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return HandleMessage(
+                scan_tag=obj["scan_tag"],
+                handle=Handle.from_json_object(obj["handle"]))
+
+
+class MetadataMessage(NamedTuple):
+    scan_tag: object
+    handle: Handle
+    metadata: dict
+
+    @classmethod
+    def from_json_object(cls, obj):
+        return MetadataMessage(
+                scan_tag=obj["scan_tag"],
+                handle=Handle.from_json_object(obj["handle"]),
+                metadata=obj["metadata"])
 
 
 class MatchesMessage(NamedTuple):
-    scan_spec: dict
+    scan_spec: ScanSpecMessage
     handle: Handle
     matched: bool
-    matches: list
+    matches: Sequence[MatchFragment]
 
     @property
     def sensitivity(self):
@@ -19,22 +113,20 @@ class MatchesMessage(NamedTuple):
             return None
         else:
 
-            def _cms(rule_result):
+            def _cms(fragment):
                 """Computes the sensitivity of a set of results returned by a
                 rule, returning (in order of preference) the highest
                 sensitivity (lower than that of the rule) associated with a
                 match, the sensitivity of the rule, or 0."""
-                rule_sensitivity = None
-                if "sensitivity" in rule_result["rule"]:
-                    rule_sensitivity = rule_result["rule"]["sensitivity"]
+                rule_sensitivity = fragment.rule.sensitivity
 
                 max_sub = None
                 if (rule_sensitivity is not None
-                        and rule_result["matches"] is not None):
+                        and fragment.matches is not None):
                     max_sub = None
-                    for match in rule_result["matches"]:
-                        if "sensitivity" in match:
-                            sub = match["sensitivity"]
+                    for match_dict in fragment.matches:
+                        if "sensitivity" in match_dict:
+                            sub = match_dict["sensitivity"]
                             if max_sub is None or sub > max_sub:
                                 max_sub = sub
                 if max_sub is not None:
@@ -45,13 +137,13 @@ class MatchesMessage(NamedTuple):
                     return rule_sensitivity
                 else:
                     return 0
-            return Sensitivity(
-                    max([_cms(rule_result) for rule_result in self.matches]))
+            return Sensitivity(max([_cms(frag) for frag in self.matches]))
 
     @staticmethod
     def from_json_object(obj):
         return MatchesMessage(
-                scan_spec=obj["scan_spec"],
+                scan_spec=ScanSpecMessage.from_json_object(obj["scan_spec"]),
                 handle=Handle.from_json_object(obj["handle"]),
                 matched=obj["matched"],
-                matches=obj["matches"])
+                matches=[MatchFragment.from_json_object(mf)
+                        for mf in obj["matches"]])
