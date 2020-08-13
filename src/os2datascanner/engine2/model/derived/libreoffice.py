ef74f508cfa0ae90e770c033127b0d228e25028b
Alexander Faithfull <af@magenta.dk>
2020-08-13 12:58:42 +0200

    [#37581] Added special Office Open XML handling to LibreOfficeSource
    libmagic is really not very good at detecting these files, so we need to
    relax one of the sanity checks a bit. On the bright side, the fact that
    we use LibreOffice's infilter parameter should keep us safe

--- src/os2datascanner/engine2/model/derived/libreoffice.py
+++ src/os2datascanner/engine2/model/derived/libreoffice.py
@@ -23,11 +23,13 @@ def libreoffice(*args):
 _actually_supported_types = {
     "application/msword": "MS Word 97",
     "application/vnd.oasis.opendocument.text": "writer8",
-    "application/vnd.openxmlformats-officedocument"
-            ".wordprocessingml.document": "Office Open XML Text",
-
     "application/vnd.ms-excel": "MS Excel 97",
     "application/vnd.oasis.opendocument.spreadsheet": "calc8",
+
+    # XXX: libmagic usually can't detect OOXML files -- see the special
+    # handling of these types in in LibreOfficeSource._generate_state
+    "application/vnd.openxmlformats-officedocument"
+            ".wordprocessingml.document": "Office Open XML Text",
     "application/vnd.openxmlformats-officedocument"
             ".spreadsheetml.sheet": "Calc Office Open XML"
 }
@@ -48,6 +50,16 @@ class LibreOfficeSource(DerivedSource):
             # ... and, just to be extra safe, we tell LibreOffice what sort of
             # file we're passing it so it can do its own sanity checks
             filter_name = _actually_supported_types.get(best_mime_guess)
+
+            # (... with special handling for OOXML files, since libmagic has
+            # problems detecting them)
+            if (not filter_name and best_mime_guess in
+                    ("application/octet-stream", "application/zip")):
+                mime_guess = self.handle.guess_type()
+                if mime_guess.startswith(
+                        "application/vnd.openxmlformats-officedocument."):
+                    filter_name = _actually_supported_types.get(mime_guess)
+
             if filter_name is not None:
                 with TemporaryDirectory() as outputdir:
                     result = libreoffice(
