c96f1e67d664b6f726a5f1e1762ad8135e4011f2
danni <danni@magenta-aps.dk>
2020-09-09 10:58:05 +0200

    [#38232] - Changed prefix for all environmental virables from DSC to OS2DS.

--- src/os2datascanner/utils/pika_settings.py
+++ src/os2datascanner/utils/pika_settings.py
@@ -8,48 +8,48 @@ from os2datascanner.utils.toml_configuration import get_3_layer_config
 logger = logging.getLogger(__file__)
 
 
-class DscModule(Enum):
+class OS2DSModule(Enum):
     ADMIN = ('admin',)
     ENGINE = ('engine',)
     REPORT = ('report',)
 
     @classmethod
-    def determine_dsc_module(cls):
+    def determine_OS2DS_module(cls):
         # TODO: Consider a more elegant way to determine this
-        if os.getenv('DSC_ADMIN_SYSTEM_CONFIG_PATH'):
+        if os.getenv('OS2DS_ADMIN_USER_CONFIG_PATH'):
             return cls.ADMIN
-        if os.getenv('DSC_ENGINE_SYSTEM_CONFIG_PATH'):
+        if os.getenv('OS2DS_ENGINE_USER_CONFIG_PATH'):
             return cls.ENGINE
-        if os.getenv('DSC_REPORT_SYSTEM_CONFIG_PATH'):
+        if os.getenv('OS2DS_REPORT_USER_CONFIG_PATH'):
             return cls.REPORT
 
     def __init__(self, _):
-        self.sys_var = f"DSC_{self.name}_SYSTEM_CONFIG_PATH"
-        self.user_var = f"DSC_{self.name}_USER_CONFIG_PATH"
+        self.sys_var = f"OS2DS_{self.name}_SYSTEM_CONFIG_PATH"
+        self.user_var = f"OS2DS_{self.name}_USER_CONFIG_PATH"
 
 
-def _get_default_settings_path(dsc_module):
+def _get_default_settings_path(OS2DS_module):
     project_dir = os.path.abspath(
         os.path.dirname(
             os.path.dirname(__file__)
         )
     )
     path = project_dir
-    if dsc_module == DscModule.ENGINE:
+    if OS2DS_module == OS2DSModule.ENGINE:
         path = os.path.join(project_dir, 'engine2')
-    elif dsc_module == DscModule.ADMIN:
+    elif OS2DS_module == OS2DSModule.ADMIN:
         path = os.path.join(project_dir, 'projects', 'admin')
-    elif dsc_module == DscModule.REPORT:
+    elif OS2DS_module == OS2DSModule.REPORT:
         path = os.path.join(project_dir, 'projects', 'report')
     return os.path.join(path, 'default-settings.toml')
 
 
 def _get_config(key=None):
-    dsc_module = DscModule.determine_dsc_module()
-    default_settings = _get_default_settings_path(dsc_module)
+    OS2DS_module = OS2DSModule.determine_OS2DS_module()
+    default_settings = _get_default_settings_path(OS2DS_module)
     config = get_3_layer_config(default_settings,
-                                sys_var=dsc_module.sys_var,
-                                user_var=dsc_module.user_var)
+                                sys_var=OS2DS_module.sys_var,
+                                user_var=OS2DS_module.user_var)
     if key:
         config = config[key]
     return config
