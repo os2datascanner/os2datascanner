a08d796e08ddab0c4a38b5167cd154fdb1dba670
Alexander Faithfull <af@magenta.dk>
2020-10-29 12:19:27 +0100

    [#39082] Added a custom argument parser action for overrideable lists
    This will be used to support the new administration system pipeline
    collector, which should receive copies of certain messages

--- src/os2datascanner/engine2/pipeline/utilities/args.py
+++ src/os2datascanner/engine2/pipeline/utilities/args.py
@@ -34,3 +34,21 @@ def make_sourcemanager_configuration_block(parser):
             default=3)
 
     return configuration
+
+
+class AppendReplaceAction(argparse.Action):
+    """AppendReplaceAction behaves like the 'append' action, but the default
+    value, if there is one, will be removed from the list the first time a
+    value is explicitly specified."""
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        self._fired = set()
+        default = kwargs.get("default")
+        if default and not isinstance(default, list):
+            kwargs["default"] = [default]
+        super().__init__(option_strings, dest, nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not id(namespace) in self._fired:
+            setattr(namespace, self.dest, [])
+            self._fired.add(id(namespace))
+        setattr(namespace, self.dest, getattr(namespace, self.dest) + [values])
