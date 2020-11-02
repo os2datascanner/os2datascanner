8c132dd3121ff86b25e104458b20edf03539aa54
Novik <ns@magenta.dk>
2020-11-02 13:18:32 +0100

    fix handle match bug

--- src/os2datascanner/projects/report/reportapp/static/src/js/handle-match.js
+++ src/os2datascanner/projects/report/reportapp/static/src/js/handle-match.js
@@ -23,6 +23,22 @@ $(document).click(function(event) {
   }
 });
 
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 $(actions).click(function() {
   const btn_action = $(this)
 
