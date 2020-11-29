7e4fe790269819aef6cddbc0b978aafe97f77820
Mikkel Rostved <mr@magenta-aps.dk>
2020-11-29 19:29:07 +0100

    [#39805] Add metadata to determine whether to broadcast model events

--- src/os2datascanner/projects/admin/adminapp/models/organization_model.py
+++ src/os2datascanner/projects/admin/adminapp/models/organization_model.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
 from uuid import uuid4
 
-class Organization(models.Model):
 
+class Organization(models.Model):
     """Represents the organization for each user and scanner, etc.
 
     Users can only see material related to their own organization.
@@ -32,3 +32,6 @@ class Organization(models.Model):
             "do_use_groups": self.do_use_groups,
             "do_notify_all_scans": self.do_notify_all_scans,
         }
+
+    class DatascannerMeta:
+        broadcast_change_events = True
--- src/os2datascanner/projects/admin/adminapp/signals.py
+++ src/os2datascanner/projects/admin/adminapp/signals.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.forms.models import model_to_dict
-from inspect import ismethod
 from os2datascanner.engine2.pipeline.utilities.pika import PikaPipelineSender
 import json
 import logging
@@ -11,16 +10,16 @@ import datetime
 
 logger = logging.getLogger(__name__)
 
+
 class ModelChangeEvent():
     publisher = "admin"
 
-    def __init__(self, event_type, model_class, instance, meta = {}):
+    def __init__(self, event_type, model_class, instance, meta={}):
         self.event_type = event_type
         self.model_class = model_class
         self.instance = instance
         self.meta = meta
         self.time = datetime.datetime.now().isoformat()
-
 
     def to_json_object(self):
         return {
@@ -32,10 +31,10 @@ class ModelChangeEvent():
             # The type of model
             "model_class": self.instance.__class__.__name__,
             # The actual model (JSON-representation using to_json_object())
-            "instance": self.instance.to_json_object() 
-                            if hasattr(self.instance, "to_json_object") and 
-                               ismethod(getattr(self.instance, "to_json_object"))
-                            else {"error": "missing to_json_object() method"},
+            "instance": self.instance.to_json_object()
+            if hasattr(self.instance, "to_json_object") and
+               callable(self.instance.to_json_object)
+            else {"error": "missing to_json_object() method"},
             "meta": self.meta
         }
 
@@ -49,22 +48,37 @@ def publish_events(events):
                 json_event = event.to_json_object()
                 logger.info("Published to {0}: {1}".format(queue, json_event))
                 pps.publish_message(queue, json_event)
-    except Exception as e: 
+    except Exception as e:
         # log the error
-        logger.error("Could not publish event. Error: "+format(e))
+        logger.error("Could not publish event. Error: " + format(e))
+
+
+def event_broadcast_enabled(obj):
+    """Determine whether event broadcasting is enabled for a given object"""
+    try:
+        enabled = obj.DatascannerMeta.broadcast_change_events
+        return enabled
+    except AttributeError:
+        return False
 
 
 @receiver(post_save)
 def post_save_callback(**kwargs):
     """Signal receiver for post_save - publishes events when objects are saved"""
-    event = ModelChangeEvent('object_create' if kwargs.get("created") else 'object_update', 
-                  kwargs.get('sender'), 
-                  kwargs.get('instance'))
+    if not event_broadcast_enabled(kwargs.get('instance')):
+        return
+
+    event = ModelChangeEvent('object_create' if kwargs.get("created") else 'object_update',
+                             kwargs.get('sender'),
+                             kwargs.get('instance'))
     publish_events([event])
 
 
 @receiver(post_delete)
 def post_delete_callback(**kwargs):
     """Signal receiver for post_delete - publishes events when objects are deleted"""
+    if not event_broadcast_enabled(kwargs.get('instance')):
+        return
+
     event = ModelChangeEvent('object_delete', kwargs.get('sender'), kwargs.get('instance'))
     publish_events([event])
