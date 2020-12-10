0d10ab58847a9434fdf98e682435552fde624fc0
danni <danni@magenta-aps.dk>
2020-12-10 10:56:13 +0100

    [#39864] - Deleted ugly json blob and enhanced unit tests.

--- src/os2datascanner/projects/report/tests/test_mainpageview.py
+++ src/os2datascanner/projects/report/tests/test_mainpageview.py
@@ -1,35 +1,172 @@
-import json
-
 from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
 
-from ..reportapp.models.documentreport_model import DocumentReport
+from os2datascanner.engine2.model.ews import (
+        EWSMailHandle, EWSAccountSource)
+from os2datascanner.engine2.rules.regex import RegexRule, Sensitivity
+from os2datascanner.engine2.pipeline import messages
+
+from ..reportapp.management.commands import pipeline_collector
 from ..reportapp.models.aliases.emailalias_model import EmailAlias
 from ..reportapp.models.roles.remediator_model import Remediator
 from ..reportapp.views.views import MainPageView
 
-scan_time = "2020-11-11T11:11:59+02:00"
-path = '4b012c5e20a6770cb0713b540f71af281eeb6f0c0b776267cc48294f13cf7cdbcf2b1642fd11da687c7c49fc11f3394eea59dcaec739072929e53dac8a16b76d'
-data = '{"matches": {"handle": {"mime": "text/html", "path": "1/", "type": "mail-part", "source": {"type": "mail", "handle": {"path": "LKSDLDSJFOEWIROEWYSMBD", "type": "ews", "source": {"type": "ews", "user": "kjeld", "domain": "jensen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Inbox", "mail_subject": "Sv: GDPR f\u00f8lsom data"}}}, "origin": "os2ds_matches", "matched": true, "matches": [{"rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "matches": null}, {"rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "matches": null}, {"rule": {"name": null, "type": "cpr", "modulus_11": true, "sensitivity": 750, "ignore_irrelevant": true}, "matches": [{"match": "1111XXXXXX", "offset": 1133, "context": "elle cpr nummer man m\u00e5 bruge som test cpr nummer: XXXXXX-XXXX Du m\u00e5 meget gerne svare p\u00e5 denne mail med noget a", "probability": 1.0, "sensitivity": 750, "context_offset": 0}]}], "scan_spec": {"rule": {"name": null, "type": "and", "components": [{"name": null, "type": "or", "components": [{"name": null, "type": "and", "components": [{"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, {"name": null, "type": "dimensions", "width": [32, 16385], "height": [32, 16385], "minimum": 128, "sensitivity": null}], "sensitivity": null}, {"name": null, "rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "type": "not", "sensitivity": null}], "sensitivity": null}, {"name": null, "type": "cpr", "modulus_11": true, "sensitivity": 750, "ignore_irrelevant": true}], "sensitivity": null}, "source": {"type": "mail", "handle": {"path": "LKSDLDSJFOEWIROEWYSMBD", "type": "ews", "source": {"type": "ews", "user": "kjeld", "domain": "jensen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Inbox", "mail_subject": "Sv: GDPR f\u00f8lsom data"}}, "progress": null, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "kjeld", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}, "configuration": {}}}, "metadata": {"handle": {"mime": "text/html", "path": "1/", "type": "mail-part", "source": {"type": "mail", "handle": {"path": "LKSDLDSJFOEWIROEWYSMBD", "type": "ews", "source": {"type": "ews", "user": "kjeld", "domain": "jensen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Inbox", "mail_subject": "Sv: GDPR f\u00f8lsom data"}}}, "origin": "os2ds_metadata", "metadata": {"email-account": "kjeld@jensen.com"}, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "kjeld", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}}, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "kjeld", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}}'
-path2 = 'ef57aa18e938ca9b11ff41408a29c9f110dd9efe4e8dd17b42ff98d7e8d6e68183880a85a4a9a0238bcd938058c742b28c0dab85e082cba96b78b5edcf80d69d'
-data2 = '{"matches": {"handle": {"mime": "text/html", "path": "1/", "type": "mail-part", "source": {"type": "mail", "handle": {"path": "TDJHGFIHDIJHSKJGHKFUGIUHIUEHIIHE", "type": "ews", "source": {"type": "ews", "user": "egon", "domain": "olsen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Indbakke", "mail_subject": "GDPR f\u00f8lsom data"}}}, "origin": "os2ds_matches", "matched": true, "matches": [{"rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "matches": null}, {"rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "matches": null}, {"rule": {"name": null, "type": "cpr", "modulus_11": true, "sensitivity": 750, "ignore_irrelevant": true}, "matches": [{"match": "1111XXXXXX", "offset": 248, "context": "elle cpr nummer man m\u00e5 bruge som test cpr nummer: XXXXXX-XXXX Du m\u00e5 meget gerne svare p\u00e5 denne mail med noget a", "probability": 1.0, "sensitivity": 750, "context_offset": 0}]}], "scan_spec": {"rule": {"name": null, "type": "and", "components": [{"name": null, "type": "or", "components": [{"name": null, "type": "and", "components": [{"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, {"name": null, "type": "dimensions", "width": [32, 16385], "height": [32, 16385], "minimum": 128, "sensitivity": null}], "sensitivity": null}, {"name": null, "rule": {"name": null, "type": "conversion", "target": "image-dimensions", "sensitivity": null}, "type": "not", "sensitivity": null}], "sensitivity": null}, {"name": null, "type": "cpr", "modulus_11": true, "sensitivity": 750, "ignore_irrelevant": true}], "sensitivity": null}, "source": {"type": "mail", "handle": {"path": "TDJHGFIHDIJHSKJGHKFUGIUHIUEHIIHE", "type": "ews", "source": {"type": "ews", "user": "egon", "domain": "olsen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Indbakke", "mail_subject": "GDPR f\u00f8lsom data"}}, "progress": null, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "egon", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}, "configuration": {}}}, "metadata": {"handle": {"mime": "text/html", "path": "1/", "type": "mail-part", "source": {"type": "mail", "handle": {"path": "TDJHGFIHDIJHSKJGHKFUGIUHIUEHIIHE", "type": "ews", "source": {"type": "ews", "user": "egon", "domain": "olsen.com", "server": "", "admin_user": null, "admin_password": null}, "folder_name": "Indbakke", "mail_subject": "GDPR f\u00f8lsom data"}}}, "origin": "os2ds_metadata", "metadata": {"email-account": "egon@olsen.com"}, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "egon", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}}, "scan_tag": {"time": "2020-11-11T11:11:59+02:00", "user": "egon", "scanner": {"pk": 6, "name": "CPR scan"}, "destination": "pipeline_collector", "organisation": "Olsenbanden ApS"}}'
+
+"""Shared data"""
+time0 = "2020-11-11T11:11:59+02:00"
+time1 = "2020-10-28T14:21:27+01:00"
+
+scan_tag0 = {
+    "scanner": "Dummy test scanner",
+    "time": time0
+}
+scan_tag1 = {
+    "scanner": "Dummy test scanner",
+    "time": time1
+}
+
+common_rule = RegexRule(
+    expression="Vores hemmelige adgangskode er",
+    sensitivity=Sensitivity.PROBLEM
+)
+
+
+"""EGON DATA"""
+egon_email_handle = EWSMailHandle(
+    source=EWSAccountSource(
+        domain='@olsen.com',
+        server=None,
+        admin_user=None,
+        admin_password=None,
+        user='egon'),
+    path='TDJHGFIHDIJHSKJGHKFUGIUHIUEHIIHE',
+    mail_subject='Jeg har en plan',
+    folder_name='Hundehoveder',
+    entry_id=None
+)
+
+egon_email_handle_1 = EWSMailHandle(
+    source=EWSAccountSource(
+        domain='@olsen.com',
+        server=None,
+        admin_user=None,
+        admin_password=None,
+        user='egon'),
+    path='DLFIGHDSLUJKGFHEWIUTGHSLJHFGBSVDKJFHG',
+    mail_subject='TI STILLE SINDSSYGE KVINDEMENNESKE!',
+    folder_name='Hundehoveder',
+    entry_id=None
+)
+
+egon_scan_spec = messages.ScanSpecMessage(
+        scan_tag=None,  # placeholder
+        source=egon_email_handle.source,
+        rule=common_rule,
+        configuration={},
+        progress=None)
+
+egon_positive_match = messages.MatchesMessage(
+        scan_spec=egon_scan_spec._replace(scan_tag=scan_tag0),
+        handle=egon_email_handle,
+        matched=True,
+        matches=[messages.MatchFragment(
+                rule=common_rule,
+                matches=[{"dummy": "match object"}])]
+)
+
+egon_positive_match_1 = messages.MatchesMessage(
+        scan_spec=egon_scan_spec._replace(scan_tag=scan_tag1),
+        handle=egon_email_handle_1,
+        matched=True,
+        matches=[messages.MatchFragment(
+                rule=common_rule,
+                matches=[{"dummy": "match object"}])]
+)
+
+egon_metadata = messages.MetadataMessage(
+    scan_tag=scan_tag0,
+    handle=egon_email_handle,
+    metadata={"email-account": "egon@olsen.com"}
+)
+
+egon_metadata_1 = messages.MetadataMessage(
+    scan_tag=scan_tag1,
+    handle=egon_email_handle_1,
+    metadata={"email-account": "egon@olsen.com"}
+)
+
+
+"""KJELD DATA"""
+kjeld_email_handle = EWSMailHandle(
+    source=EWSAccountSource(
+        domain='@jensen.com',
+        server=None,
+        admin_user=None,
+        admin_password=None,
+        user='kjeld'),
+    path='TDJHGFIHDIJHSKJGHKFUGIUHIUEHIIHE',
+    mail_subject='Er det farligt?',
+    folder_name='Indbakke',
+    entry_id=None
+)
+
+kjeld_scan_spec = messages.ScanSpecMessage(
+    scan_tag=None,  # placeholder
+    source=kjeld_email_handle.source,
+    rule=common_rule,
+    configuration={},
+    progress=None)
+
+kjeld_positive_match = messages.MatchesMessage(
+        scan_spec=kjeld_scan_spec._replace(scan_tag=scan_tag0),
+        handle=kjeld_email_handle,
+        matched=True,
+        matches=[messages.MatchFragment(
+                rule=common_rule,
+                matches=[{"dummy": "match object"}])]
+)
+
+kjeld_metadata = messages.MetadataMessage(
+    scan_tag=scan_tag0,
+    handle=kjeld_email_handle,
+    metadata={"email-account": "kjeld@jensen.com"}
+)
 
 
 class MainPageViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        number_of_kjeld_matches = 7
-        cls.generate_matches(number_of_kjeld_matches, path, data)
-        number_of_egon_matches = 5
-        cls.generate_matches(number_of_egon_matches, path2, data2)
+        cls.generate_kjeld_data()
+        cls.generate_egon_data()
 
     @classmethod
-    def generate_matches(cls, number_of_matches, path, data):
-        for number in range(number_of_matches):
-            DocumentReport.objects.create(scan_time=scan_time,
-                                          path=path,
-                                          data=json.loads(data),
-                                          resolution_status=None)
+    def generate_kjeld_data(cls):
+        cls.generate_match(kjeld_positive_match)
+        cls.generate_metadata(kjeld_metadata)
+
+    @classmethod
+    def generate_egon_data(cls):
+        cls.generate_match(egon_positive_match)
+        cls.generate_metadata(egon_metadata)
+
+        cls.generate_match(egon_positive_match_1)
+        cls.generate_metadata(egon_metadata_1)
+
+    @classmethod
+    def generate_match(cls, match):
+        prev, new = pipeline_collector.get_reports_for(
+            match.handle.to_json_object(),
+            match.scan_spec.scan_tag)
+        pipeline_collector.handle_match_message(
+            prev, new, match.to_json_object())
+
+    @classmethod
+    def generate_metadata(cls, metadata):
+        prev, new = pipeline_collector.get_reports_for(
+            metadata.handle.to_json_object(),
+            metadata.scan_tag)
+        pipeline_collector.handle_metadata_message(
+            new, metadata.to_json_object())
 
     def setUp(self):
         # Every test needs access to the request factory.
@@ -44,20 +181,26 @@ class MainPageViewTest(TestCase):
     def test_mainpage_view_as_remediator_role_with_matches(self):
         remediator = Remediator.objects.create(user=self.user)
         qs = self.mainpage_get_queryset()
-        self.assertEqual(len(qs), 12)
+        self.assertEqual(len(qs), 3)
         remediator.delete()
 
-    def test_mainpage_view_with_emailalias_with_matches(self):
-        emailalias = EmailAlias.objects.create(user=self.user, address='kjeld@jensen.com')
+    def test_mainpage_view_with_emailalias_egon(self):
+        emailalias = EmailAlias.objects.create(user=self.user, address='egon@olsen.com')
         qs = self.mainpage_get_queryset()
-        self.assertEqual(len(qs), 7)
+        self.assertEqual(len(qs), 2)
         emailalias.delete()
 
-    def test_mainpage_view_with_two_emailaliases_with_matches(self):
+    def test_mainpage_view_with_emailalias_kjeld(self):
+        emailalias = EmailAlias.objects.create(user=self.user, address='kjeld@jensen.com')
+        qs = self.mainpage_get_queryset()
+        self.assertEqual(len(qs), 1)
+        emailalias.delete()
+
+    def test_mainpage_view_with_emailaliases_egon_kjeld(self):
         emailalias = EmailAlias.objects.create(user=self.user, address='kjeld@jensen.com')
         emailalias1 = EmailAlias.objects.create(user=self.user, address='egon@olsen.com')
         qs = self.mainpage_get_queryset()
-        self.assertEqual(len(qs), 12)
+        self.assertEqual(len(qs), 3)
         emailalias.delete()
         emailalias1.delete()
 
