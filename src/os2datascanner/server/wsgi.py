71ae7ccf733729547cf3992779f7259829dcc545
Alexander Faithfull <af@magenta.dk>
2020-12-14 15:33:09 +0100

    [#36693] Added a dummy method and some error handling to the API host
    The basics of the API here are modelled on those of the report module, with
    the idea that we might want to do some code sharing (and then replacing) at
    some point

--- src/os2datascanner/server/wsgi.py
+++ src/os2datascanner/server/wsgi.py
@@ -3,7 +3,43 @@ import json
 from . import settings
 
 
+def dummy_1(body):
+    yield "200 OK"
+    yield {
+        "status": "ok"
+    }
+
+
+def error_1(body):
+    yield "400 Bad Request"
+    yield {
+        "status": "fail",
+        "message": "action was missing or did not identify an endpoint"
+    }
+
+
+def catastrophe_1(body):
+    yield "400 Really Very Bad Request Indeed"
+    yield {
+        "status": "fail",
+        "message": "payload was not a valid JSON object"
+    }
+
+
+api_endpoints = {
+    "dummy-1": dummy_1
+}
+
+
 def application(env, start_response):
-    start_response("200 OK", [
+    try:
+        body = json.loads(env["wsgi.input"].read().decode("ascii"))
+        it = api_endpoints.get(body.get("action"), error_1)(body)
+    except json.JSONDecodeError:
+        it = catastrophe_1(None)
+
+    status = next(it)
+    start_response(status, [
             ("Content-Type", "application/json")])
-    yield(json.dumps({"status": "error"}).encode("ascii"))
+    for obj in it:
+        yield json.dumps(obj).encode("ascii")
