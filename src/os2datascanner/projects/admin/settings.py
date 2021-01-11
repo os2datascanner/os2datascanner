9a263bde6ea89127882f028955272feb0c15010b
Novik <ns@magenta.dk>
2021-01-11 14:46:34 +0100

    [#38632] Use timezone

--- src/os2datascanner/projects/admin/settings.py
+++ src/os2datascanner/projects/admin/settings.py
@@ -19,6 +19,8 @@ process_toml_conf_for_django(
     user_var='OS2DS_ADMIN_USER_CONFIG_PATH',
 )
 
+USE_TZ = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
