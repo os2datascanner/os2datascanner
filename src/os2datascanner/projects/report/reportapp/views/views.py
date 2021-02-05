04e2a248466baf7541859ed8fcf1bf838d08ec24
mb <mb@magenta-aps.dk>
2021-02-05 10:57:11 +0100

    [#41297] Added chart.js library and removed duplicate code for getting hold of user roles.
    This code has been changed in an earlier commit, but rebase merge conflict was not corrected correctly.

--- src/os2datascanner/projects/report/reportapp/views/views.py
+++ src/os2datascanner/projects/report/reportapp/views/views.py
@@ -111,8 +111,7 @@ class MainPageView(ListView, LoginRequiredMixin):
             )
 
         # matches are always ordered by sensitivity desc. and probability desc.
-        return self.matches, self.roles
-
+        return self.matches
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
