02e9e9371d11637dab01cbbe2dc997c09ae7d072
Alexander Faithfull <af@magenta.dk>
2021-02-12 13:06:24 +0100

    [#41429] Add metadata extraction to libreoffice.py
    This commit has a few side-effects; chief amongst them is the fact that
    there's a new, non-synthetic, LibreOfficeObjectResource class to expose the
    metadata. This has implications for the test suite, which now also tests
    PDF metadata in order to check that synthetic files are skipped

--- src/os2datascanner/engine2/tests/test_engine2_metadata.py
+++ src/os2datascanner/engine2/tests/test_engine2_metadata.py
@@ -6,18 +6,28 @@ from os2datascanner.engine2.model.core import Handle, Source, SourceManager
 from os2datascanner.engine2.model.file import (
         FilesystemHandle, FilesystemSource)
 from os2datascanner.engine2.model.http import (WebHandle, WebSource)
+from os2datascanner.engine2.model.derived.pdf import (
+        PDFSource, PDFPageHandle, PDFPageSource, PDFObjectHandle)
 from os2datascanner.engine2.model.derived.libreoffice import (
-        LibreOfficeObjectHandle, LibreOfficeSource)
+        LibreOfficeSource, LibreOfficeObjectHandle)
 
 
 here_path = os.path.dirname(__file__)
 test_data_path = os.path.join(here_path, "data")
-test_handle = LibreOfficeObjectHandle(
+odt_test_handle = LibreOfficeObjectHandle(
         LibreOfficeSource(
                 FilesystemHandle(
                         FilesystemSource(test_data_path),
                         "libreoffice/embedded-cpr.odt")),
         "embedded-cpr.html")
+pdf_test_handle = PDFObjectHandle(
+        PDFPageSource(
+                PDFPageHandle(
+                        PDFSource(
+                                FilesystemHandle(
+                                        FilesystemSource(test_data_path),
+                                        "pdf/embedded-cpr.pdf")), "1")),
+        "page.txt")
 
 
 class CountingProxy:
@@ -34,18 +44,24 @@ class CountingProxy:
 
 
 class MetadataTest(unittest.TestCase):
-    def setUp(self):
-        self.handle_proxy = CountingProxy(test_handle)
-
     def test_odt_extraction(self):
         with SourceManager() as sm:
-            metadata = guess_responsible_party(self.handle_proxy, sm)
+            metadata = guess_responsible_party(odt_test_handle, sm)
         self.assertEqual(
-                metadata["od-modifier"],
+                metadata["od-creator"],
+                "Alexander John Faithfull",
+                "metadata extraction failed")
+
+    def test_pdf_extraction(self):
+        proxy = CountingProxy(pdf_test_handle)
+        with SourceManager() as sm:
+            metadata = guess_responsible_party(proxy, sm)
+        self.assertEqual(
+                metadata["pdf-author"],
                 "Alexander John Faithfull",
                 "metadata extraction failed")
         self.assertEqual(
-                self.handle_proxy.get_attr_access_count("follow"),
+                proxy.get_attr_access_count("follow"),
                 0,
                 "metadata extraction from synthetic file attempted")
 
--- src/os2datascanner/utils/metadata.py
+++ src/os2datascanner/utils/metadata.py
@@ -1,102 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 from sys import stderr
-from codecs import lookup as lookup_codec
-import olefile
-from zipfile import ZipFile, BadZipFile
 from traceback import print_exc
-from defusedxml.ElementTree import parse
 
-from os2datascanner.engine2.model.core import FileResource
-from os2datascanner.engine2.model.ews import EWSMailResource
-from os2datascanner.engine2.model.http import WebResource
-from os2datascanner.engine2.model.file import FilesystemResource
-from os2datascanner.engine2.model.smbc import SMBCResource
-from os2datascanner.engine2.model.derived.pdf import PDFPageResource
-
-
-def _codepage_to_codec(cp):
-    """Retrieves the Python text codec corresponding to the given Windows
-    codepage."""
-
-    # In principle, this isn't hard: Python has lots of inbuilt codecs with
-    # predictable names ("cp" followed by the string representation of the
-    # codepage number), but...
-    try:
-        # ... codepage 65001 is an interesting special case: it's "whatever
-        # WideCharToMultiByte returns when you ask it for UTF-8" (which is,
-        # at least in some versions of Windows, known not to be proper UTF-8).
-        # As a result, Python can only expose it as a codec on Windows! For
-        # now, we treat it as normal UTF-8...
-        if cp == 65001:
-            return lookup_codec("utf-8")
-        else:
-            # All codepages must be represented with at least three digits:
-            # this is only a problem for "cp037", but we may as well keep
-            # things general
-            return lookup_codec("cp{:03}".format(cp))
-    except LookupError:
-        return None
-
-def _get_ole_metadata(fp):
-    try:
-        raw = olefile.OleFileIO(fp).get_metadata()
-
-        # Check that the codepage attribute has been set. If it hasn't, then
-        # the document didn't define a SummaryInformation stream and so doesn't
-        # have the metadata we want
-        if raw.codepage is not None:
-            tidied = {}
-            # The value we get here is a signed 16-bit quantity, even though
-            # the file format specifies values up to 65001
-            tidied["codepage"] = raw.codepage
-            if tidied["codepage"] < 0:
-                tidied["codepage"] += 65536
-            codec = _codepage_to_codec(tidied["codepage"])
-            if codec:
-                for name in olefile.OleMetadata.SUMMARY_ATTRIBS:
-                    if name in tidied:
-                        continue
-                    value = getattr(raw, name)
-                    if isinstance(value, bytes):
-                        value, _ = codec.decode(value)
-                    tidied[name] = value
-            return tidied
-        else:
-            return None
-    # CodecInfo.decode raises a ValueError (or a subclass) on failure
-    except (FileNotFoundError, ValueError):
-        return None
-
-def _process_zip_resource(fp, member, func):
-    try:
-        with ZipFile(fp, "r") as z:
-            with z.open(member, "r") as f:
-                return func(f)
-    except (KeyError, BadZipFile, FileNotFoundError):
-        return None
-
-def _check_dictionary_field(d, k, value=None):
-    """Many of the metadata extraction functions in this file return dictionary
-    or dictionary-like objects on success and None on failure -- and, depending
-    on the precise structure of the underlying metadata, these objects may or
-    may not contain any particular named fields.
-
-    This utility function provides a simple way of checking for the presence of
-    fields in these return values without explicitly having to check for None
-    values at any point."""
-    return d and k in d and d[k] is not None and (not value or value == d[k])
-
-def type_is_ole(mime):
-    return mime in ("application/msword", "application/vnd.ms-excel",
-            "application/vnd.ms-powerpoint")
-
-def type_is_ooxml(mime):
-    return mime.startswith("application/vnd.openxmlformats-officedocument.")
-
-def type_is_opendocument(mime):
-    return mime.startswith("application/vnd.oasis.opendocument.")
 
 def guess_responsible_party(handle, sm):
     """Returns a dictionary of labelled speculations about the person
@@ -138,62 +44,7 @@ def guess_responsible_party(handle, sm):
             # there is any metadata here, it'll be higher up the tree
             return
 
-        resource = handle.follow(sm)
-        is_derived = bool(handle.source.handle)
-
-        if (not is_derived
-                and isinstance(resource,
-                        (WebResource, EWSMailResource, FilesystemResource,
-                        SMBCResource, PDFPageResource))):
-            yield from resource._generate_metadata()
-
-        # Extract content metadata
-        if isinstance(resource, FileResource):
-            media_type = handle.guess_type()
-
-            if type_is_opendocument(media_type):
-                # Extract OpenDocument metadata
-                f = None
-                with resource.make_stream() as fp:
-                    f = _process_zip_resource(fp, "meta.xml", parse)
-                if f:
-                    content = f.find(
-                            "{urn:oasis:names:tc:opendocument:"
-                            "xmlns:office:1.0}meta")
-                    if content:
-                        lm = content.find(
-                                "{http://purl.org/dc/elements/1.1/}creator")
-                        if lm is not None and lm.text:
-                            yield "od-modifier", lm.text.strip()
-                        c = content.find(
-                                "{urn:oasis:names:tc:opendocument:"
-                                "xmlns:meta:1.0}initial-creator")
-                        if c is not None and c.text:
-                            yield "od-creator", c.text.strip()
-            elif type_is_ooxml(media_type):
-                # Extract Office Open XML metadata
-                f = None
-                with resource.make_stream() as fp:
-                    f = _process_zip_resource(fp, "docProps/core.xml", parse)
-                if f:
-                    lm = f.find(
-                            "{http://schemas.openxmlformats.org/package/"
-                            "2006/metadata/core-properties}lastModifiedBy")
-                    if lm is not None and lm.text:
-                        yield "ooxml-modifier", lm.text.strip()
-                    c = f.find("{http://purl.org/dc/elements/1.1/}creator")
-                    if c is not None and c.text:
-                        yield "ooxml-creator", c.text.strip()
-            elif type_is_ole(media_type):
-                # Extract old Microsoft office document metadata
-                m = None
-                with resource.make_stream() as fp:
-                    m = _get_ole_metadata(fp)
-                if m:
-                    if "last_saved_by" in m:
-                        yield "ole-modifier", m["last_saved_by"]
-                    if "author" in m:
-                        yield "ole-creator", m["author"]
+        yield from handle.follow(sm)._generate_metadata()
 
     guesses = {}
     # Files in the real world can be malformed in a wide array of exciting
