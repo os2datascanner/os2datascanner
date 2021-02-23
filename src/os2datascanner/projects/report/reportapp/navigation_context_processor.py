bade340b27b0f4a0aea9797d9ba73f3cee6745dc
apb <apb@magenta-aps.dk>
2021-02-23 10:28:47 +0100

    [#41721] Fix annonymous user handling

--- src/os2datascanner/projects/report/reportapp/navigation_context_processor.py
+++ src/os2datascanner/projects/report/reportapp/navigation_context_processor.py
@@ -2,8 +2,11 @@ from .models.roles.defaultrole_model import DefaultRole
 
 
 def navigation_items(request):
-    user = request.user
-    roles = user.roles.select_subclasses() or [DefaultRole(user=user)]
-    return {
-       'nav_items': [role.__class__.__name__ for role in roles],
-    }
+   if request.user.is_authenticated:
+      user = request.user
+      roles = user.roles.select_subclasses() or [DefaultRole(user=user)]
+      return {
+         'nav_items': [role.__class__.__name__ for role in roles],
+      }
+   else:
+      return {'nav_items': 'No roles'}
