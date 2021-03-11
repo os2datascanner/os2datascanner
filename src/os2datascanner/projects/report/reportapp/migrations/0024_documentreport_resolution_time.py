8c556af9e57541360a86192b224ad8fa7562d11d
danni <danni@magenta-aps.dk>
2021-03-11 09:44:42 +0100

    [#38449] Migrations have been re-arranged, so they do not break development.

--- src/os2datascanner/projects/report/reportapp/migrations/0024_documentreport_resolution_time.py
+++ src/os2datascanner/projects/report/reportapp/migrations/0024_documentreport_resolution_time.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('os2datascanner_report', '0023_documentreport_indexing_for_performance'),
+        ('os2datascanner_report', '0023_adds_role_dataprotection_officer'),
     ]
 
     operations = [
