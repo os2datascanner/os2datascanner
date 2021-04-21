75c07609d2c9f7164c563a4ba076ba760f0824e6
Jonas Kofoed Hansen <jkh@magenta-aps.dk>
2021-04-21 15:38:56 +0200

    [#43117] - Cron.py argument fix 	* "now" argument for manage.py cron 	now has boolean value instead of string value 	* --now tag now works according to intend 	* Scannerjobs will by default be skipped if they should not start yet

--- src/os2datascanner/projects/admin/adminapp/management/commands/cron.py
+++ src/os2datascanner/projects/admin/adminapp/management/commands/cron.py
@@ -44,7 +44,7 @@ class Command(BaseCommand):
        parser.add_argument(
              "--now",
             metavar="now",
-            default="False",
+            default=False,
             help="Run the scanner now if scheduled for today")
 
     def handle(self, *args, now=False, **kwargs):
