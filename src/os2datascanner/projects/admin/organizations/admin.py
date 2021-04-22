1cb542ad8e4e63b716fb45cbea5ddeecd44e43ef
IBP <ibp@magenta.dk>
2021-04-22 16:17:37 +0200

    [#43138] Fix AliasTypes and add Alias to db

--- src/os2datascanner/projects/admin/organizations/admin.py
+++ src/os2datascanner/projects/admin/organizations/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Organization, OrganizationalUnit, Account, Position
+from .models import Organization, OrganizationalUnit, Account, Position, Alias
 
 # Register your models here.
 
-for model in [Organization, OrganizationalUnit, Account, Position]:
+for model in [Organization, OrganizationalUnit, Account, Position, Alias]:
     admin.site.register(model)
--- src/os2datascanner/projects/admin/organizations/models/__init__.py
+++ src/os2datascanner/projects/admin/organizations/models/__init__.py
@@ -1,5 +1,6 @@
 # Import needed here for django models:
 from .account import Account
+from .aliases import Alias
 from .organizational_unit import OrganizationalUnit
 from .organization import Organization
 from .position import Position
