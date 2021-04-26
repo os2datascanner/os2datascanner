038f979a8b6f24cb6a4532ea3a30dd06c00b83c7
Emil Thorenfeldt <emt@magenta-aps.dk>
2021-04-26 14:17:23 +0200

    [#42948] Document qualweb in README, add test script using it

---
+++ services/a11y-scanner/tests/smoke_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ROOT_DIR_FALLBACK=$(git rev-parse --show-toplevel)
+ROOT_DIR=${CI_PROJECT_DIR:-$ROOT_DIR_FALLBACK}
+
+A11Y_DIR="${ROOT_DIR}/services/a11y-scanner"
+A11Y_PORT=8888
+
+# Load credentials
+# shellcheck disable=SC1090
+source "${ROOT_DIR}/dev-environment/a11y-scanner/a11y-scanner.env"
+
+curl --user "$A11Y_SCANNER_USERNAME:$A11Y_SCANNER_PASSWORD" \
+	-H "Content-Type: application/json" \
+	--data-binary "@${A11Y_DIR}/tests/request_bodies/magenta.json" \
+	http://localhost:${A11Y_PORT}/qualweb
