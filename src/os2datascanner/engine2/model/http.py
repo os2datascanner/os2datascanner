b51c8167973595a698b9e76c556a7f3195297021
Paw Møller <pawsen@gmail.com>
2021-05-26 15:46:00 +0200

    [#43727] Set loglevel for engine components

--- src/os2datascanner/engine2/model/http.py
+++ src/os2datascanner/engine2/model/http.py
@@ -308,7 +308,8 @@ def make_outlinks(content, where):
         for el, _, li, _ in doc.iterlinks():
             if el.tag in ("a", "img",):
                 yield li
-    except ParserError:
+    except ParserError as e:
         # Silently drop ParserErrors, but only for empty documents
         if content and not content.isspace():
-            logging.exception("{0}: unexpected ParserError".format(where))
+            logger.error("{0}: unexpected ParserError".format(where),
+                         exc_info=True)
