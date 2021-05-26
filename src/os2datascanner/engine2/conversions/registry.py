e351b03cb05c8d3ad2505c1f6f2e4130c1216299
Paw Møller <pawsen@gmail.com>
2021-05-26 13:03:37 +0200

    [#43727] rename conversion `output_type` parameter
    The docstring says the converter, converts from `mime_type` to
    `OutputType` which makes sense. `input_type` is confusing.

--- src/os2datascanner/engine2/conversions/registry.py
+++ src/os2datascanner/engine2/conversions/registry.py
@@ -4,12 +4,12 @@ from .utilities.results import SingleResult
 __converters = {}
 
 
-def conversion(input_type, *mime_types):
+def conversion(output_type, *mime_types):
     """Decorator: registers the decorated function as the converter of each of
     the specified MIME types to the specified OutputType."""
     def _conversion(f):
-        def _register_converter(input_type, mime_type):
-            k = (input_type, mime_type)
+        def _register_converter(output_type, mime_type):
+            k = (output_type, mime_type)
             if k in __converters:
                 raise ValueError(
                         "BUG: can't register two handlers for"
@@ -18,29 +18,29 @@ def conversion(input_type, *mime_types):
                 __converters[k] = f
         if mime_types:
             for m in mime_types:
-                _register_converter(input_type, m)
+                _register_converter(output_type, m)
         else:
-            _register_converter(input_type, None)
+            _register_converter(output_type, None)
         return f
     return _conversion
 
 
-def convert(resource, input_type, mime_override=None) -> SingleResult:
+def convert(resource, output_type, mime_override=None) -> SingleResult:
     """Tries to convert a Resource to the specified OutputType by using the
     database of registered conversion functions.
 
     Raises a KeyError if no conversion exists."""
     mime_type = resource.compute_type() if not mime_override else mime_override
     try:
-        converter = __converters[(input_type, mime_type)]
+        converter = __converters[(output_type, mime_type)]
     except KeyError as e:
         try:
-            converter = __converters[(input_type, None)]
+            converter = __converters[(output_type, None)]
         except KeyError:
             # Raise the original, more specific, exception
             raise KeyError("No converters registered for "
                            "{0}".format(e)) from  e
     value = converter(resource)
     if value is not None and not isinstance(value, SingleResult):
-        value = SingleResult(None, input_type, value)
+        value = SingleResult(None, output_type, value)
     return value
