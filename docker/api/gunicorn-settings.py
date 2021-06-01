8e9b4238690a74ffbab9a29e67a4bc38cbb921be
Mathias Dannesbo (Magenta) <md@magenta.dk>
2021-06-01 11:05:30 +0200

    [#43837] Revert gunicorn bump and settings file
    This reverts commit 97b409d62f2882dbc9951b228946b7cb176d79e6 and
    232e371bb0b86c4d11521e18c50f07337d64c297 from
    https://git.magenta.dk/os2datascanner/os2datascanner/-/merge_requests/606 .
    
    It resulted in the application returning `Error: No application module
    specified.` and chrashing.
    
    The root cause is unknown.

--- docker/api/gunicorn-settings.py
+++ docker/api/gunicorn-settings.py
@@ -5,18 +5,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+################################################################################
+# Changes to this file requires approval from Labs. Please add a person from   #
+# Labs as required approval to your MR if you have any changes.                #
+################################################################################
+
+
 # Settings for gunicorn in docker.
 import multiprocessing
 import os
 
-wsgi_app = "os2datascanner.server.wsgi"
 
 bind = "0.0.0.0:5000"
 worker_class = "gevent"
 workers = os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
-# default directory for heartbeat file is in /tmp, which in some Linux distros is
-# stored in memory via tmpfs filesystem. Docker containers, however, do not have
+# default directory for heartbeat file is in /tmp, which in some Linux distros is 
+# stored in memory via tmpfs filesystem. Docker containers, however, do not have 
 # /tmp on tmpfs by default - so we use /dev/shm
 # https://pythonspeed.com/articles/gunicorn-in-docker/
-worker_tmp_dir = "/dev/shm"
-accesslog = "-"
+worker_tmp_dir = "/dev/shm" 
+accesslog =  "-"
