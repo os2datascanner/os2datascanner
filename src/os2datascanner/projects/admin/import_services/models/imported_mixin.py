9570dd53dce1855d8182c67d8920adf02aaa01f0
danni <danni@magenta-aps.dk>
2021-06-30 10:38:40 +0200

    [#44363] - Changed imported_id charfield to textfield.

--- src/os2datascanner/projects/admin/import_services/models/imported_mixin.py
+++ src/os2datascanner/projects/admin/import_services/models/imported_mixin.py
@@ -33,8 +33,7 @@ class Imported(models.Model):
     )
 
     # Store unique ID from external service
-    imported_id = models.CharField(
-        max_length=256,
+    imported_id = models.TextField(
         null=True,
         blank=True,
         verbose_name=_('imported unique ID'),
