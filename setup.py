d51d3e42c32618dec6b83c8feb2cda9ab470e705
Paw Møller <pawsen@gmail.com>
2021-07-27 11:48:48 +0200

    [#44504] prometheus sends version info & delete VERSION file

--- setup.py
+++ setup.py
@@ -8,11 +8,18 @@ def all_lines_of(*project_relative_path_fragments):
             .parent.joinpath(*project_relative_path_fragments)
             .read_text().splitlines())
 
+
 setuptools.setup(
     install_requires=(
         all_lines_of("requirements",
                 "python-requirements", "requirements-admin.txt") +
         all_lines_of("requirements",
                 "python-requirements", "requirements-engine.txt")
-    )
+    ),
+    package_data={"os2datascanner": [
+        "engine2/default-settings.toml",
+        "projects/admin/default-settings.toml",
+        "projects/report/default-settings.toml",
+        "server/default-settings.toml",
+    ]},
 )
