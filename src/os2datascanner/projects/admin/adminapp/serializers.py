b547ee93dc52d83005f61b6bb6aab154b72db853
ewh <ewh@magenta-aps.dk>
2021-09-14 10:43:10 +0200

    [#45560] Show saved OU's on load and restructure JS

--- src/os2datascanner/projects/admin/adminapp/serializers.py
+++ src/os2datascanner/projects/admin/adminapp/serializers.py
@@ -4,4 +4,5 @@ from ..organizations.models import OrganizationalUnit
 class OrganizationalUnitSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = OrganizationalUnit
-		fields = ('uuid', 'name', 'lft', 'rght', 'tree_id', 'level', 'parent', 'organization')
+		fields = ('uuid', 'name', 'tree_id', 'level', 'parent',
+				  'organization', 'exchangescanners')
--- src/os2datascanner/projects/admin/adminapp/static/js/getOrgUnits.js
+++ src/os2datascanner/projects/admin/adminapp/static/js/getOrgUnits.js
@@ -9,7 +9,7 @@ $(function() {
     var callOnChange = new MutationObserver(function() {
         if (parameters['organization_id'] != document.querySelector('#id_organization').value){  
             parameters['organization_id'] = document.querySelector('#id_organization').value
-            
+
             //When the organization is called, getorgunit.js removes the selected values, 
             //but not the already selected values from the variable in dropdowntree.js,
             //instead of adding another eventlistener on the same dropdown as this, we set the value to empty here
@@ -22,53 +22,74 @@ $(function() {
         characterData: false, 
         attributes: true, 
         childList: true, 
-        subtree: false 
+        subtree: false }
+    );    
+});
+
+function getOrgUnits() {
+    var url = $('#sel_1').attr("url");
+    $.ajax({
+        method: 'GET',
+        url: url,
+        data: parameters,
+        success: function (result) {
+            insertData(result);
+        },
+        error: function () {
+            console.error("Something went wrong!");
+        }
     });
-    
-    function getOrgUnits() {
-        var url = $('#sel_1').attr("url");
-        $.ajax({
-            method: 'GET',
-            url: url,
-            data: parameters,
-            success: function (result) {
-                insertData(result);
-            },
-            error: function (response) {
-                console.log(response);
-            }
-        });
-    }
+}
 
-    // Function for restructuring object to fit format of select2ToTree
-    function treeify(list, idAttr, parentAttr, childrenAttr) {
-        if (!idAttr) idAttr = 'uuid';
-        if (!parentAttr) parentAttr = 'parent';
-        if (!childrenAttr) childrenAttr = 'inc';
-       
-        var treeList = [];
-        var lookup = {};
-        list.forEach(function(obj) {
-            lookup[obj[idAttr]] = obj;
-            obj[childrenAttr] = [];
-        });
-        list.forEach(function(obj) {
-            if (obj[parentAttr] != null) {
-                if (lookup[obj[parentAttr]] !== undefined) {
-                    lookup[obj[parentAttr]][childrenAttr].push(obj);
-                } else {
-                     treeList.push(obj);
-                }               
+// Function for restructuring object to fit format of select2ToTree
+function treeify(list, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'uuid';
+    if (!parentAttr) parentAttr = 'parent';
+    if (!childrenAttr) childrenAttr = 'inc';
+   
+    var treeList = [];
+    var lookup = {};
+    list.forEach(function(obj) {
+        lookup[obj[idAttr]] = obj;
+        obj[childrenAttr] = [];
+    });
+    list.forEach(function(obj) {
+        if (obj[parentAttr] != null) {
+            if (lookup[obj[parentAttr]] !== undefined) {
+                lookup[obj[parentAttr]][childrenAttr].push(obj);
             } else {
-                treeList.push(obj);
-            }
-        });
-        return treeList;
-    };
+                 treeList.push(obj);
+            }               
+        } else {
+            treeList.push(obj);
+        }
+    });
+    return treeList;
+};
 
-    function insertData(result) {
-        var treeArray = treeify(result)
-        $("#sel_1").empty();
-        $("#sel_1").select2ToTree({treeData: {dataArr:treeArray}})
+function isOrgUnitSelected(org_units) {
+    // Empty the function when it's called, to make sure it's only called once
+    // since we don't want to "refill" the select box, everytime a user changes organization
+    isOrgUnitSelected = function(){}
+    
+    // determine wether the orginazational unit is selected
+    // do not use this on add new
+    if(!document.location.pathname.indexOf('add') != -1){
+        var scannerJobId = document.location.pathname.split('/')[2]
+        for(var i = 0; i < org_units.length; i++){
+            for(var j = 0;j < org_units[i]['exchangescanners'].length;j++) {
+                if(org_units[i]['exchangescanners'][j] == scannerJobId) {
+                    org_units[i]['selected'] = "true"
+                    break;
+                }
+            }
+        }
     }
-})
+}
+
+function insertData(result) {
+    isOrgUnitSelected(result)
+    var treeArray = treeify(result)
+    $("#sel_1").empty();
+    $("#sel_1").select2ToTree({treeData: {dataArr:treeArray}})
+}
