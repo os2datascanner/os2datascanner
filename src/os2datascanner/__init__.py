e9274d7c17a7edadb1afff3b9f2460c98b1dcbfa
Paw Møller <pawsen@gmail.com>
2021-09-30 16:00:11 +0200

    set default struclog log-level to warning
    This prevents structlog emitting debug-messages, if DS is called without
    `run_stage`.
    
    Ie. silence these log-messages
    > python -m os2datascanner.engine2.demo.url_explorer https://magenta.dk
    2021-09-30 15:18.22 handle parsed for links        handle=https://www.magenta.dk/ links=125
    https://www.magenta.dk/
    https://www.magenta.dk/wp-content/uploads/2019/03/cropped-magenta_logo-2.png
    2021-09-30 15:18.22 handle parsed for links        handle=https://www.magenta.dk/huset-magenta-it-leverandor-danmark-siden-1999/ links=229
    https://www.magenta.dk/huset-magenta-it-leverandor-danmark-siden-1999/
    2021-09-30 15:18.24 handle parsed for links        handle=https://www.magenta.dk/leverancemodel-agil-it-udvikling-analyse-kvalitet/ links=84
    https://www.magenta.dk/leverancemodel-agil-it-udvikling-analyse-kvalitet/

--- src/os2datascanner/__init__.py
+++ src/os2datascanner/__init__.py
@@ -1,6 +1,8 @@
 from . import engine2  # noqa
 from . import utils  # noqa
 import logging
+import structlog
+from structlog.stdlib import LoggerFactory
 
 __version__ = "3.11.6"
 
@@ -10,3 +12,6 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 # logging.getLogger('os2datascanner').propagate = False
 
 logging.getLogger("pika").setLevel(logging.WARNING)
+
+# prevent structlog from emitting all log-statements if no loglevel is set.
+structlog.configure(logger_factory=LoggerFactory())
