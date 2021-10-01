bf76f695f3c3e2b90c9243baace0115ef003842a
Toke Fritzemeier <toke@magenta.dk>
2021-10-01 14:01:38 +0200

    [#45569] Make match handling row sticky.

--- src/os2datascanner/projects/report/reportapp/static/src/js/index.js
+++ src/os2datascanner/projects/report/reportapp/static/src/js/index.js
@@ -12,12 +12,3 @@ import './results.js';
 
 // Copy Path function
 new ClipboardJS(document.querySelectorAll('[data-clipboard-text]'));
-
-//Change background color after scroll
-$(window).on("scroll", function() {
-  if($(window).scrollTop() > 200) {
-      $(".datatable th").addClass("scrollActive");
-  } else {
-     $(".datatable th").removeClass("scrollActive");
-  }
-});
