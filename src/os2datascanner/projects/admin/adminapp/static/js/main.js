b3d106c1478fa7127c32d8aec4eed8dc23c5cedf
Toke Fritzemeier <toke@magenta.dk>
2021-10-15 15:35:10 +0200

    Fixup: Removed unused JS code.

--- src/os2datascanner/projects/admin/adminapp/static/js/main.js
+++ src/os2datascanner/projects/admin/adminapp/static/js/main.js
@@ -40,28 +40,15 @@ os2web = window.os2web || {};
         }
     });
 })(os2web, jQuery);
-/* jshint -W098 */ //disable check for 'is used'
-// declared globally so we can access it from separate IIFEs.
-function handleSubChoices(choice) {
-  var state = choice.prop("checked");
-  var siblings = choice.siblings("input[type='checkbox']");
-  siblings.prop("disabled", !state);
-  if(!state) {
-    siblings.prop("checked", false).change();
-  }
-}
-
-function recalcIframeHeight() { // we need to do this every time we add/remove an item from the rule list
-  var thisBodyHeight = $("body").height();
-  var parentIframe = $(".modal-body iframe", window.parent.document);
-  parentIframe.height(thisBodyHeight);
-}
 
 /* debounce function from https://davidwalsh.name/javascript-debounce-function */
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
+// this method is used globally, so instruct JSHint to ignore W098 which enforces
+// that functions must be used
+/* jshint -W098 */
 function os2debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
@@ -80,3 +67,4 @@ function os2debounce(func, wait, immediate) {
         }
 	};
 }
+/* jshint +W098 */
