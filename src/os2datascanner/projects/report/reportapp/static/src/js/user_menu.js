23c6616c62955d7ef04d57d5ce8c2c33951db37b
Toke Fritzemeier <toke@magenta.dk>
2021-10-15 15:38:09 +0200

    [#45915] Basic responsive styling including responsive navigation menu.
    [#45915] Tables + login/password flow screens + user menu now responsive.
    
    [#45915] Carry over responsification changes from admin module to report module.
    
    [#45915] Adjustments to datatable styling in report view (impossible to make scrollable *and* have a sticky header).
    
    [#45915] IE11 specific fixes to logo and flexbox layout.
    
    [#45915] Make pagination responsive.
    
    [#45915] Hide matchcount & source columns at small viewport widths.
    
    [#45915] Make file upload styling correspond to text input styling.
    
    [#45915] Read the select2 widget width from the element instead of setting 100%, which the plugin can't correctly compute a pixel value from.
    
    [#45915] Make navigation tabs more usable by disabling scrolling interaction and instead wrapping across several lines.
    
    [#45915] Make responsive tables keyboard scrollable and signify this with inner shadow.
    
    [#45915] Stack responsive menu correctly on top of filter options in report module.
    
    [#45915] Make rule selector behave properly across display sizes.
    
    [#45915] Make header sticky on small screen sizes + fix datatable sticky headers and background in report module.
    
    [#45915] Custom selects shouldn't overflow

--- src/os2datascanner/projects/report/reportapp/static/src/js/user_menu.js
+++ src/os2datascanner/projects/report/reportapp/static/src/js/user_menu.js
@@ -1,3 +1,4 @@
+/* jshint -W098 */ //disable check is used ( called from html )
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropMenu() {
@@ -12,4 +13,20 @@ window.onclick = function(e) {
         dropdown.classList.remove('show');
       }
   }
-}
+};
+
+// Toggle side navigation on small display sizes
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("navigation-toggle").addEventListener("click", function (e) {
+    var menu = document.getElementById("sidemenu");
+    var toggle = e.target;
+    var isHidden = !toggle.hasAttribute("aria-expanded") || toggle.getAttribute("aria-expanded") === "false";
+    if (isHidden) {
+      menu.style.display = "block";
+      toggle.setAttribute("aria-expanded", "");
+    } else {
+      menu.style.display = "none";
+      toggle.removeAttribute("aria-expanded");
+    }
+  });
+});
