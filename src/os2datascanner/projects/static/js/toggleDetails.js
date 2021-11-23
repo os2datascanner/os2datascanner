da015ececf469a50f5f6b8fa450b1b0aa7be4f49
Toke Fritzemeier <toke@magenta.dk>
2021-11-23 13:44:39 +0100

    [#46913] Change icons in start scan modal, as the old icons were not displaying for some reason.

--- src/os2datascanner/projects/static/js/toggleDetails.js
+++ src/os2datascanner/projects/static/js/toggleDetails.js
@@ -2,16 +2,16 @@
 // toggle scan details
 function toggleDetails() {
   var x = document.getElementById("scan-details");
-  var down = document.getElementById("arrow_drop_down");
-  var right = document.getElementById("arrow_right");
+  var down = document.getElementById("expand_more");
+  var right = document.getElementById("chevron_right");
 
   if (x.style.display === "none") {
     x.style.display = "block";
-    down.style.display = "block";
+    down.style.display = "inline";
     right.style.display = "none";
   } else {
     x.style.display = "none";
     down.style.display = "none";
-    right.style.display = "block";
+    right.style.display = "inline";
   }
 }
