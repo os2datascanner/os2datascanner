a526ca055f2190572fe95b869392e3f1c67f651a
Alexander Faithfull <af@magenta.dk>
2021-12-01 13:11:00 +0100

    [#47335] Add a test for the treatment of null characters in PDF metadata
    This test fails, which is sad. In a moment it will not

--- src/os2datascanner/engine2/tests/test_engine2_metadata.py
+++ src/os2datascanner/engine2/tests/test_engine2_metadata.py
@@ -6,41 +6,45 @@ from os2datascanner.engine2.model.file import (
         FilesystemHandle, FilesystemSource)
 from os2datascanner.engine2.model.http import (WebHandle, WebSource)
 from os2datascanner.engine2.model.derived.pdf import (
-        PDFSource, PDFPageHandle, PDFPageSource, PDFObjectHandle)
+        PDFPageHandle, PDFObjectHandle)
 from os2datascanner.engine2.model.derived.libreoffice import (
         LibreOfficeSource, LibreOfficeObjectHandle)
 
 
-here_path = os.path.dirname(__file__)
-test_data_path = os.path.join(here_path, "data")
-odt_test_handle = LibreOfficeObjectHandle(
-        LibreOfficeSource(
-                FilesystemHandle(
-                        FilesystemSource(test_data_path),
-                        "libreoffice/embedded-cpr.odt")),
-        "embedded-cpr.html")
-pdf_test_handle = PDFObjectHandle(
-        PDFPageSource(
-                PDFPageHandle(
-                        PDFSource(
-                                FilesystemHandle(
-                                        FilesystemSource(test_data_path),
-                                        "pdf/embedded-cpr.pdf")), "1")),
-        "page.txt")
+test_data = FilesystemSource(os.path.join(os.path.dirname(__file__), "data"))
 
 
 class MetadataTest(unittest.TestCase):
     def test_odt_extraction(self):
+        handle = LibreOfficeObjectHandle(
+                LibreOfficeSource(
+                        FilesystemHandle(
+                                test_data, "libreoffice/embedded-cpr.odt")),
+                "embedded-cpr.html")
         with SourceManager() as sm:
-            metadata = odt_test_handle.follow(sm).get_metadata()
+            metadata = handle.follow(sm).get_metadata()
         self.assertEqual(
                 metadata["od-creator"],
                 "Alexander John Faithfull",
                 "metadata extraction failed")
 
     def test_pdf_extraction(self):
+        handle = PDFObjectHandle.make(
+                FilesystemHandle(test_data, "pdf/embedded-cpr.pdf"),
+                1, "page.txt")
         with SourceManager() as sm:
-            metadata = pdf_test_handle.follow(sm).get_metadata()
+            metadata = handle.follow(sm).get_metadata()
+        self.assertEqual(
+                metadata["pdf-author"],
+                "Alexander John Faithfull",
+                "metadata extraction failed")
+
+    def test_weird_pdf_metadata(self):
+        """Null bytes in PDF metadata should be automatically removed."""
+        handle = PDFPageHandle.make(
+                FilesystemHandle(test_data, "pdf/null-byte-in-author.pdf"), 1)
+        with SourceManager() as sm:
+            metadata = handle.follow(sm).get_metadata()
         self.assertEqual(
                 metadata["pdf-author"],
                 "Alexander John Faithfull",
