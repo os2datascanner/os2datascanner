0dc54133f457b5a96a86e3fd199d176b95fd4a63
Alexander Faithfull <af@magenta.dk>
2021-12-17 09:03:23 +0100

    [#44703] Add a new API for performing operations with backoff
    run_with_backoff has served us... sort of okay, I suppose?... but is
    clunky, hard to understand, and inflexible. This commit introduces a new
    bunch of classes that implement the same logic in a (hopefully) more
    modular way

--- src/os2datascanner/engine2/utilities/backoff.py
+++ src/os2datascanner/engine2/utilities/backoff.py
@@ -6,6 +6,124 @@ import structlog
 logger = structlog.get_logger(__name__)
 
 
+class Retrier:
+    """A Retrier is a helper object used to repeat certain operations when a
+    transient exception is raised. A concrete instance of Retrier or of one of
+    its subclasses is a stateful object representing a strategy for repeat
+    execution."""
+    def __init__(self, *exception_set: Exception):
+        self._exception_set = tuple(exception_set)
+
+    @property
+    def _should_proceed(self):
+        """Indicates whether or not the state of this Retrier indicates that
+        another attempt should be made."""
+        return True
+
+    def _should_retry(self, ex: Exception) -> bool:
+        """Examines an exception to determine whether or not it represents a
+        transient error."""
+        return isinstance(ex, self._exception_set)
+
+    def _before_retry(self, ex: Exception, op):
+        """Called after _should_retry has indicated that an exception is
+        transient, but before _should_proceed has been called for the next
+        attempt. (The operation being executed is made available for the sake
+        of debug logging.)
+
+        This is the main hook method of the Retrier class. Subclasses might
+        want to extend it to update internal state or to insert a delay."""
+        pass
+
+    def run(self, operation, *args, **kwargs):
+        """Repeatedly calls operation(*args, **kwargs) until it returns without
+        raising a transient error. Returns whatever that function eventually
+        returns.
+
+        Subclasses may wish to extend this method to, for example, reset parts
+        of their internal state before calling this implementation."""
+        while self._should_proceed:
+            try:
+                return operation(*args, **kwargs)
+            except Exception as ex:
+                if self._should_retry(ex):
+                    self._before_retry(ex, operation)
+                    if self._should_proceed:
+                        continue
+                raise
+
+    def bind(self, operation):
+        def _bind(*args, **kwargs):
+            return self.run(operation, *args, **kwargs)
+        return _bind
+
+
+class CountingRetrier(Retrier):
+    """A CountingRetrier tracks the number of attempts made to call an
+    operation, and gives up after a certain number."""
+    def __init__(self, *exception_set, max_tries=10, warn_after=6, **kwargs):
+        super().__init__(*exception_set, **kwargs)
+        self._tries = 0
+        self._warn_after = warn_after
+        self._max_tries = max_tries
+
+    @property
+    def _should_proceed(self):
+        return self._max_tries is None or self._tries < self._max_tries
+
+    def _before_retry(self, ex, op):
+        self._tries += 1
+        if self._warn_after is not None and self._tries >= self._warn_after:
+            logger.info(
+                "backoff failed, delaying",
+                op=str(op),
+                iterations=self._tries
+            )
+
+    def run(self, operation, *args, **kwargs):
+        self._tries = 0
+        return super().run(operation, *args, **kwargs)
+
+
+class SleepingRetrier(CountingRetrier):
+    """A SleepingRetrier is a CountingRetrier that also adds a delay after
+    each attempt. (This is by default a static delay of one second, but
+    subclasses can specify something more sophisticated by overriding the
+    _compute_delay method.)"""
+
+    def _compute_delay(self):
+        return 1.0
+
+    def _before_retry(self, ex, op):
+        super()._before_retry(ex, op)
+        # Only bother delaying if there's going to *be* a next attempt
+        if self._should_proceed:
+            sleep(self._compute_delay())
+
+
+class ExponentialBackoffRetrier(SleepingRetrier):
+    def __init__(self, *exception_set, base=1, ceiling=7, fuzz=0.2, **kwargs):
+        super().__init__(*exception_set, **kwargs)
+        self._base = base
+        self._fuzz = 0
+        self._ceiling = ceiling
+
+    def _compute_delay(self):
+        max_delay = self._base * (2 ** min(self._tries, self._ceiling) - 1)
+        if self._fuzz:
+            fuzz_diff = (max_delay * self._fuzz)
+            adj = -fuzz_diff + (2 * random() * fuzz_diff)
+            max_delay += adj
+        return max_delay
+
+
+DefaultRetrier = ExponentialBackoffRetrier
+"""DefaultRetrier is an alias for whichever Retrier the project considers to be
+the best choice at any given point. If you just want to retry a function a
+reasonable number of times with sensible default behaviour and don't need any
+particular tuning parameters, then instantiate one of these."""
+
+
 def run_with_backoff(  # noqa: CCR001, too high cognitive complexity
         op, *exception_set,
         count=0, max_tries=10, ceiling=7, base=1, warn_after=6, fuzz=0):
