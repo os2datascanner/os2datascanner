f5fe8bf659feb7e0e2097d7d06f62d4a2e56bfba
Alexander Faithfull <af@magenta.dk>
2022-02-08 16:07:08 +0100

    [#48419] Add the rest of the SMB protocol's file attributes
    This set is not *quite* the same as the Windows file attributes, so this
    commit also notes where there are holes in the sequence

--- src/os2datascanner/engine2/model/smbc.py
+++ src/os2datascanner/engine2/model/smbc.py
@@ -36,13 +36,22 @@ class Mode(enum.IntFlag):
     """A convenience enumeration for manipulating SMB file mode flags."""
     # description of flags mapping
     # https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-smb/65e0c225-5925-44b0-8104-6b91339c709f
-    # https://github.com/samba-team/samba/blob/master/source3/include/libsmbclient.h#L200
+    NONE = 0x0
     READ_ONLY = 0x01
     HIDDEN = 0x02
     SYSTEM = 0x04
-    VOLUME_ID = 0x08
+    # Windows defines VOLUME_ID = 0x08, but CIFS/SMB doesn't
     DIRECTORY = 0x10
     ARCHIVE = 0x20
+    # Windows defines DEVICE = 0x40, but CIFS/SMB doesn't
+    NORMAL = 0x80
+    TEMPORARY = 0x100
+    SPARSE = 0x200
+    REPARSE_POINT = 0x400
+    COMPRESSED = 0x800
+    OFFLINE = 0x1000
+    NONINDEXED = 0x2000
+    ENCRYPTED = 0x4000
 
     @staticmethod
     def for_url(context: smbc.Context, url: str) -> Optional['Mode']:
