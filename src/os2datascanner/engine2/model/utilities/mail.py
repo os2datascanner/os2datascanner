a2afb41814ad45d33dd9a1553b9c6ef07d121384
Alexander Faithfull <af@magenta.dk>
2022-05-25 15:18:11 +0200

    [#49709] Add support for decoding Encoded-Word values for presentation

---
+++ src/os2datascanner/engine2/model/utilities/mail.py
@@ -0,0 +1,21 @@
+from email.header import decode_header
+
+
+def decode_encoded_words(k: str) -> str:
+    """Decodes a string that might contain Encoded-Word values."""
+    result = ""
+    for segment, charset in decode_header(k):
+        segment_str = None
+        if isinstance(segment, str) and charset is None:
+            segment_str = segment
+        elif isinstance(segment, bytes):
+            try:
+                segment_str = segment.decode(charset or "ascii")
+            except LookupError:
+                # The character set of this segment isn't recognised. Decode it
+                # it as ASCII and replace unknown bytes with placeholders
+                segment_str = segment.decode(
+                        encoding="ascii", errors="replace")
+        if segment_str:
+            result += segment_str
+    return result
--- src/os2datascanner/engine2/tests/test_engine2_mail.py
+++ src/os2datascanner/engine2/tests/test_engine2_mail.py
@@ -7,9 +7,27 @@ from os2datascanner.engine2.model.file import (
 from os2datascanner.engine2.model.derived.mail import (
         MailSource, MailPartHandle)
 
+from os2datascanner.engine2.model.utilities.mail import decode_encoded_words
+
 
 here_path = os.path.dirname(__file__)
 test_data_path = os.path.join(here_path, "data", "mail")
+
+
+test_subject = """SV: Ændret købsaftale på plads"""
+
+
+encoded_subjects = {
+    "utf-8-base64": "=?utf-8?b?U1Y6IMOGbmRyZXQga8O4YnNhZnRhbGUgcMOlIHBsYWRz?=",
+    "latin-1-q": "=?iso-8859-1?q?SV=3A_=C6ndret_k=F8bsaftale_p=E5_plads?=",
+    "utf-8-q-split": (
+            "=?utf-8?q?SV=3A_=C3=86ndret?="
+            "=?utf-8?q?_k=C3=B8bsaftale_p=C3=A5_plads?="
+    ),
+    "mixed-q-split": (
+            "=?iso-8859-1?q?SV=3A_=C6ndret?="
+            "=?utf-8?q?_k=C3=B8bsaftale_p=C3=A5_plads?=")
+}
 
 
 class Engine2MailTest(TestCase):
@@ -36,3 +54,12 @@ class Engine2MailTest(TestCase):
                     len(list(alternative_source.handles(sm))),
                     1,
                     "text/plain trimming failed")
+
+    def test_encoded_word_decoding(self):
+        """Encoded-Word values can be decoded."""
+        for sort, value in encoded_subjects.items():
+            with self.subTest():
+                self.assertEqual(
+                        test_subject,
+                        decode_encoded_words(value),
+                        f"decoding {sort} failed")
