55596ed05686c012f4c5330e79234e40e03fc508
Tomas Hagenau Andersen <tha@magenta-aps.dk>
2022-06-22 15:07:39 +0200

    [#49660] Lock the database with transaction.atomic() and select_for_update()

--- src/os2datascanner/projects/admin/adminapp/management/commands/rotate_keys.py
+++ src/os2datascanner/projects/admin/adminapp/management/commands/rotate_keys.py
@@ -2,6 +2,7 @@
 from tqdm import tqdm
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 # from os2datascanner.projects.admin.import_services.models.keycloak import KeycloakServer
 from os2datascanner.projects.admin.import_services.models.ldap_configuration import LDAPConfig
@@ -37,50 +38,57 @@ class Command(BaseCommand):
             f"New DECRYPTION_HEX: {new_hex}"
         ))
 
-        # Query the database for all objects with encrypted data.
-        # keycloaks = KeycloakServer.objects.all()
-        ldap_configs = LDAPConfig.objects.all()
-        authentications = Authentication.objects.all()
+        try:
+            with transaction.atomic():
+                # Query the database for all objects with encrypted data.
+                # keycloaks = KeycloakServer.objects.all()
+                ldap_configs = LDAPConfig.objects.select_for_update()
+                authentications = Authentication.objects.select_for_update()
 
-        count = ldap_configs.count() + authentications.count()
+                count = ldap_configs.count() + authentications.count()
 
-        # Check that there are some objects to update
-        if count > 0:
-            self.stdout.write(self.style.WARNING(
-                "Make sure you take a note of the above key! " +
-                "It will not be printed again and\nit is not recoverable - without it" +
-                " ALL ENCRYPTED PASSWORDS WILL BE LOST."
-            ))
+                # Check that there are some objects to update
+                if count > 0:
+                    self.stdout.write(self.style.WARNING(
+                        "Make sure you take a note of the above key! " +
+                        "It will not be printed again and\nit is not recoverable - without it" +
+                        " ALL ENCRYPTED PASSWORDS WILL BE LOST."))
 
-            if options.get('--dry-run'):
-                # Just output the number of objects to update if the '--dry-run' flag is used.
-                self.stdout.write(f"Info: {count} password(s) would be re-encrypted.")
-            else:
-                # Ask for confirmation
-                choice = input("Continue? [y/N]: ")
+                    if options.get('--dry-run'):
+                        # Just output the number of objects to update if the '--dry-run' flag is
+                        # used.
+                        self.stdout.write(f"Info: {count} password(s) would be re-encrypted.")
+                    else:
+                        # Ask for confirmation
+                        choice = input("Continue? [y/N]: ")
 
-                if choice.lower() == "y":
-                    self.stdout.write("Initiating reencryption.")
-                    # Query the database, reencrypt the data and update the progres bar
-                    with tqdm(total=count, desc="Reencrypting") as bar:
-                        for conf in ldap_configs:
-                            conf.rotate_credential(key=bytes.fromhex(new_hex))
-                            bar.update()
+                        if choice.lower() == "y":
+                            self.stdout.write("Initiating reencryption.")
+                            # Query the database, reencrypt the data and update the progres bar
+                            with tqdm(total=count, desc="Reencrypting") as bar:
+                                for conf in ldap_configs:
+                                    conf.rotate_credential(key=bytes.fromhex(new_hex))
+                                    bar.update()
+                                    conf.save()
 
-                        for auth in authentications:
-                            password = auth.get_password()
-                            auth.set_password(password=password, key=bytes.fromhex(new_hex))
-                            bar.update()
+                                for auth in authentications:
+                                    password = auth.get_password()
+                                    auth.set_password(password=password, key=bytes.fromhex(new_hex))
+                                    bar.update()
+                                    auth.save()
 
-                    self.stdout.write(self.style.SUCCESS(
-                        f"Done. {count} password(s) re-encrypted."
-                    ))
+                            self.stdout.write(self.style.SUCCESS(
+                                f"Done. {count} password(s) re-encrypted."
+                                ))
+                        else:
+                            self.stdout.write("Process abandoned. Exiting.")
                 else:
-                    self.stdout.write("Process abandoned. Exiting.")
-        else:
-            # Otherwise don't do anything
-            self.stdout.write("No encrypted values in the database - nothing to do")
+                    # Otherwise don't do anything
+                    self.stdout.write("No encrypted values in the database - nothing to do")
 
-        self.stdout.write(self.style.WARNING(
-            "Remember to change Django's DECRYPTION_HEX setting to the new key."
-        ))
+                self.stdout.write(self.style.WARNING(
+                        "Remember to change Django's DECRYPTION_HEX setting to the new key."))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(
+                f"An error occured while trying to re-encrypt password(s): "
+                f"{e}.\nProcess abandoned. Exiting."))
