599c554839d3c2e249d52d54aca2a915eaa0b9d3
Tomas Hagenau Andersen <tha@magenta-aps.dk>
2022-06-28 13:20:07 +0200

    [#44560] Use a html file to benchmark with.

--- src/os2datascanner/engine2/tests/benchmarks/test_benchmark_cprrule.py
+++ src/os2datascanner/engine2/tests/benchmarks/test_benchmark_cprrule.py
@@ -1,11 +1,28 @@
 """Benchmarking for CPRRule."""
+import codecs
 
 from os2datascanner.engine2.rules.cpr import CPRRule
 
+CONTENT_PATH = '/code/src/os2datascanner/engine2/tests/data/html/Midler-til-frivilligt-arbejde.html'
+
+
+def read_content():
+    """Helper function that reads some content into memory."""
+    content = ""
+    with codecs.open(CONTENT_PATH, "r",
+                     encoding="utf-8",
+                     errors="ignore") as file_pointer:
+        content = file_pointer.read()
+
+    return content
+
+
+CONTENT = read_content()
+
 
 def test_benchmark_cpr_rule_no_context(benchmark):
-    """"""
-    content = ""
+    """Test performance on CPRRule without enabling examine_context."""
+    content = CONTENT
     rule = CPRRule(modulus_11=True,
                    ignore_irrelevant=False,
                    examine_context=False)
@@ -13,8 +30,8 @@ def test_benchmark_cpr_rule_no_context(benchmark):
 
 
 def test_benchmark_cpr_rule_with_context(benchmark):
-    """"""
-    content = ""
+    """Test performance on CPRRule with examine_context enabled."""
+    content = CONTENT
     rule = CPRRule(modulus_11=True,
                    ignore_irrelevant=False,
                    examine_context=True)
