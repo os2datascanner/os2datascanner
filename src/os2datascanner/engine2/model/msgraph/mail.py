f7968c1431eaae92064d5f260f947bbd2839c0a4
Tomas Hagenau Andersen <tha@magenta-aps.dk>
2022-07-11 08:51:12 +0200

    [#51462] Implement MailFSBuilder to recursively fetch mail folders.

--- src/os2datascanner/engine2/model/msgraph/mail.py
+++ src/os2datascanner/engine2/model/msgraph/mail.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from io import BytesIO
 from urllib.parse import urlsplit
 from contextlib import contextmanager
@@ -9,7 +8,7 @@ from ...conversions.utilities.results import SingleResult
 from ... import settings as engine2_settings
 from ..core import Handle, Source, Resource, FileResource
 from ..derived.derived import DerivedSource
-from .utilities import MSGraphSource, ignore_responses
+from .utilities import MSGraphSource, ignore_responses, MailFSBuilder
 
 
 class MSGraphMailSource(MSGraphSource):
@@ -112,63 +111,25 @@ class MSGraphMailAccountSource(DerivedSource):
     def handles(self, sm):
         pn = self.handle.relative_path
         ps = engine2_settings.model["msgraph"]["page_size"]
-
-        folder_map = self._generate_folder_map(sm)
+        builder = MailFSBuilder(self, sm, pn)
 
         result = sm.open(self).get(
             f"users/{pn}/messages?$select=id,subject,webLink,parentFolderId&$top={ps}")
 
-        yield from (self._wrap(msg, folder_map) for msg in result["value"])
+        yield from (self._wrap(msg, builder) for msg in result["value"])
         # We want to get all emails for given account
         # This key takes us to the next page and is only present
         # as long as there is one.
         while '@odata.nextLink' in result:
             result = sm.open(self).follow_next_link(result["@odata.nextLink"])
-            yield from (self._wrap(msg, folder_map) for msg in result["value"])
+            yield from (self._wrap(msg, builder) for msg in result["value"])
 
-    def _generate_folder_map(self, sm):
-        folder_map = {}
-        pn = self.handle.relative_path
-        ps = engine2_settings.model["msgraph"]["page_size"]
-
-        result = sm.open(self).get(
-            f"users/{pn}/mailFolders?$select=id,parentFolderId,displayName&$top={ps}")
-
-        for folder in result["value"]:
-            folder_map[folder["id"]] = _MailFolder(folder["id"],
-                                                   folder["parentFolderId"],
-                                                   folder["displayName"])
-
-        while '@odata.nextLink' in result:
-            result = sm.open(self).follow_next_link(result["@odata.nextLink"])
-            for folder in result["value"]:
-                folder_map[folder["id"]] = _MailFolder(folder["id"],
-                                                       folder["parentFolderId"],
-                                                       folder["displayName"])
-
-        return folder_map
-
-    def _wrap(self, message, folder_map):
+    def _wrap(self, message, builder: MailFSBuilder):
         fid = message["parentFolderId"]
-        folder = self._construct_folder_path(fid, folder_map)
+        folder = builder.build_path(fid)
         return MSGraphMailMessageHandle(
             self, message["id"], mail_subject=message["subject"],
             weblink=message["webLink"], folder=folder)
-
-    def _construct_folder_path(self, fid, folder_map):
-        folder = folder_map.get(fid, None)
-
-        def _reverse_traverse(f):
-            if f is None:
-                return ""
-
-            parent = folder_map.get(f._parent_folder_id, None)
-            if parent is None:
-                return f._display_name
-
-            return _reverse_traverse(parent) + '/' + f._display_name
-
-        return _reverse_traverse(folder)
 
     @staticmethod
     @Source.url_handler("test-msgraph")
@@ -181,14 +142,6 @@ class MSGraphMailAccountSource(DerivedSource):
                 MSGraphMailAccountHandle(
                         MSGraphMailSource(client_id, tenant_id, client_secret),
                         user))
-
-
-@dataclass
-class _MailFolder:
-    def __init__(self, fid, parent_folder_id, display_name):
-        self._id = fid
-        self._parent_folder_id = parent_folder_id
-        self._display_name = display_name
 
 
 class MSGraphMailMessageResource(FileResource):
@@ -258,7 +211,7 @@ class MSGraphMailMessageHandle(Handle):
 
     @property
     def presentation_place(self):
-        return f"{self._folder} of {str(self.source.handle)}"
+        return f"\"{self._folder}\" of {str(self.source.handle)}"
 
     @property
     def presentation_url(self):
