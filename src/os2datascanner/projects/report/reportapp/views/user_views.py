1131c5f6c47d9e7247c88f32ff167129f311163e
Jesper <jdk@magenta-aps.dk>
2022-07-27 08:52:37 +0200

    [#51599] Added user profile page.
    [#51599] Translations.
    
    [#51599] Removed 'general information'
    
    [#51599] Added more info.
    
    [#51599] Now displays aliases.
    
    [#51645] The report module shouldn't create missing deleted objects
    
    [#51651] Pin flake8 & remove build stage needs
    * Also corrected a small docs typo.
    
    [#51647] Added hidden input.

---
+++ src/os2datascanner/projects/report/reportapp/views/user_views.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# The contents of this file are subject to the Mozilla Public License
+# Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#    http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# OS2datascanner was developed by Magenta in collaboration with OS2 the
+# Danish community of open source municipalities (https://os2.eu/).
+#
+# The code is currently governed by OS2 the Danish community of open
+# source municipalities ( https://os2.eu/ )
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+from ..models.userprofile import User
+
+
+class UserView(TemplateView, LoginRequiredMixin):
+    template_name = "user.html"
+    context_object_name = "user"
+    model = User
+
+    def get_queryset(self):
+        return User.objects.get(username=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_roles"] = [role._meta.verbose_name for role in User.objects.get(
+            username=self.request.user).roles.select_subclasses().all()]
+        context["aliases"] = User.objects.get(username=self.request.user).aliases.all()
+        return context
