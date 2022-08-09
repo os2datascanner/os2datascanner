b500882bc5d0a6b9baade4aff3f7044240277b72
Jesper <jdk@magenta-aps.dk>
2022-08-09 11:55:31 +0200

    [#51742] HOTFIX: Refactored javascript calls.

--- src/os2datascanner/projects/static/js/options.js
+++ src/os2datascanner/projects/static/js/options.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+function setDropdownEvent() {
   Array.prototype.forEach.call(document.querySelectorAll('.dropdown > select'), function (dropdown) {
     dropdown.addEventListener('change', function (e) {
       if (e.target.hasAttribute('data-autosubmit')) {
@@ -9,4 +9,26 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+}
+
+function clearFilter(elementid) { // jshint ignore:line
+  var element = document.querySelector('#' + elementid + ' [value="all"]').selected = true;
+  document.getElementById("filter_form", element).submit();
+}
+function checkedBox() {
+  var checkbox = document.getElementById('30-days-toggle');
+  var thirtyDays = document.getElementById('30-days');
+  if (checkbox.checked) {
+    thirtyDays.value = 'true';
+  } else {
+    thirtyDays.value = 'false';
+  }
+  document.getElementById("filter_form").submit();
+}
+function setCheckEvent() { // jshint ignore:line
+  document.getElementById('30-days-toggle').addEventListener('click', checkedBox);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setDropdownEvent();
 });
