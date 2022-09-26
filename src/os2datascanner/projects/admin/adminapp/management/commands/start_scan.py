b2a190f0790c4fbbe593366719ce3acd127668cc
Alexander Faithfull <af@magenta.dk>
2022-09-26 11:46:37 +0200

    [#45449] Extend the start_scan command with checkup-only scan support

--- src/os2datascanner/projects/admin/adminapp/management/commands/start_scan.py
+++ src/os2datascanner/projects/admin/adminapp/management/commands/start_scan.py
@@ -1,6 +1,7 @@
 import sys
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from os2datascanner.projects.admin.adminapp.models.scannerjobs.scanner import (
         Scanner)
@@ -9,20 +10,24 @@ from os2datascanner.projects.admin.adminapp.models.scannerjobs.scanner import (
 class Command(BaseCommand):
     """Schedule a scanner job for execution by the pipeline, just as the user
     interface's "Run" button does."""
-    help = __doc__
+    help = ugettext(__doc__)
 
     def add_arguments(self, parser):
         parser.add_argument(
             "id",
             type=int,
-            help="the primary key of the scanner job to start",
+            help=_("the primary key of the scanner job to start"),
             default=None)
+        parser.add_argument(
+            "--checkups-only",
+            help=_("only scan objects previously scheduled for a checkup"),
+            action="store_true")
 
-    def handle(self, id, *args, **options):
+    def handle(self, id, *args, checkups_only, **options):
         try:
             scanner = Scanner.objects.select_subclasses().get(pk=id)
         except ObjectDoesNotExist:
-            print(f"no scanner job exists with id {id}")
+            print(_("no scanner job exists with id {id}").format(id=id))
             sys.exit(1)
 
-        print(scanner.run())
+        print(scanner.run(explore=not checkups_only))
