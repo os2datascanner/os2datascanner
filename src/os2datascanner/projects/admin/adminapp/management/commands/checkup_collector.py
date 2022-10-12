0ad67a8f5f29d93b199294aadb7a454e23aa4dc8
Jonas Kofoed Hansen <jkh@magenta-aps.dk>
2022-10-12 09:36:39 +0200

    [#52872] Divide and conquer collectors

--- src/os2datascanner/projects/admin/adminapp/management/commands/checkup_collector.py
+++ src/os2datascanner/projects/admin/adminapp/management/commands/checkup_collector.py
@@ -15,110 +15,23 @@
 # The code is currently governed by OS2 the Danish community of open
 # source municipalities ( https://os2.eu/ )
 
-from django.db.models import F
-from django.db import transaction
-from django.db.utils import DataError
 from django.core.management.base import BaseCommand
-from django.conf import settings
-from django.utils import timezone
 import logging
 import structlog
-import math
 
 from os2datascanner.engine2.rules.last_modified import LastModifiedRule
 from os2datascanner.engine2.pipeline import messages
 from os2datascanner.engine2.pipeline.run_stage import _loglevels
+from prometheus_client import Summary
+
 from os2datascanner.engine2.pipeline.utilities.pika import PikaPipelineThread
-
-from prometheus_client import Summary, start_http_server
-
 from ...models.scannerjobs.scanner import (
-    Scanner, ScanStatus, ScheduledCheckup, ScanStatusSnapshot)
+    Scanner, ScanStatus, ScheduledCheckup)
 from ...models.usererrorlog import UserErrorLog
-from ...notification import send_mail_upon_completion
 
 logger = structlog.get_logger(__name__)
-SUMMARY = Summary("os2datascanner_pipeline_collector_admin",
-                  "Messages through admin collector")
-
-
-def check_if_scan_completed(scan_status):
-    if scan_status.finished:
-        # Update last_modified for scanner (but only if we actually did
-        # explore some sources...)
-        if scan_status.total_sources != 0:
-            scan_status.scanner.e2_last_run_at = scan_status.last_modified
-            scan_status.scanner.save()
-        # Send email upon scannerjob completion
-        logger.info("Sending notification mail for finished scannerjob.")
-        send_mail_upon_completion(scan_status.scanner, scan_status)
-
-
-def status_message_received_raw(body):
-    """A status message for a scannerjob is created in Scanner.run().
-    Therefore this method can focus merely on updating the ScanStatus object."""
-    message = messages.StatusMessage.from_json_object(body)
-
-    try:
-        scanner = Scanner.objects.get(pk=message.scan_tag.scanner.pk)
-    except Scanner.DoesNotExist:
-        # This is a residual message for a scanner that the administrator has
-        # deleted. Throw it away
-        return
-
-    locked_qs = ScanStatus.objects.select_for_update(
-        of=('self',)
-    ).filter(
-        scanner=scanner,
-        scan_tag=body["scan_tag"]
-    )
-    # Queryset is evaluated immediately with .first() to lock the database entry.
-    locked_qs.first()
-
-    if message.total_objects is not None:
-        # An explorer has finished exploring a Source
-        locked_qs.update(
-                message=message.message,
-                last_modified=timezone.now(),
-                status_is_error=message.status_is_error,
-                total_objects=F('total_objects') + message.total_objects,
-                total_sources=F('total_sources') + (message.new_sources or 0),
-                explored_sources=F('explored_sources') + 1)
-
-    elif message.object_size is not None and message.object_type is not None:
-        # A worker has finished processing a Handle
-        locked_qs.update(
-                message=message.message,
-                last_modified=timezone.now(),
-                status_is_error=message.status_is_error,
-                scanned_size=F('scanned_size') + message.object_size,
-                scanned_objects=F('scanned_objects') + 1)
-
-    # Get the frequency setting and decide whether or not to create a snapshot
-    snapshot_param = settings.SNAPSHOT_PARAMETER
-    scan_status = locked_qs.first()
-    if scan_status:
-        n_total = scan_status.total_objects
-        if n_total and n_total > 0:
-            # Calculate a frequency for how often to take a snapshot.
-            # n_total must be a least 2 for this to work.
-            frequency = n_total * math.log(snapshot_param, max(n_total, 2))
-            # Decide whether it is time to take a snapshot.
-            take_snap = scan_status.scanned_objects % max(1, math.floor(frequency))
-            if take_snap == 0:
-                ScanStatusSnapshot.objects.create(
-                    scan_status=scan_status,
-                    time_stamp=timezone.now(),
-                    total_sources=scan_status.total_sources,
-                    explored_sources=scan_status.explored_sources,
-                    total_objects=scan_status.total_objects,
-                    scanned_objects=scan_status.scanned_objects,
-                    scanned_size=scan_status.scanned_size,
-                )
-
-        check_if_scan_completed(scan_status)
-
-    yield from []
+SUMMARY = Summary("os2datascanner_checkup_collector_admin",
+                  "Messages through checkup collector")
 
 
 def problem_message_recieved_raw(body):
@@ -286,35 +199,6 @@ def update_scheduled_checkup(handle, matches, problem, scan_time, scanner):  # n
                 handle=handle.presentation)
 
 
-class CollectorRunner(PikaPipelineThread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        start_http_server(9091)
-
-    def handle_message(self, routing_key, body):
-        with SUMMARY.time():
-            logger.debug(
-                    "raw message received",
-                    routing_key=routing_key,
-                    body=body)
-            try:
-                with transaction.atomic():
-                    if routing_key == "os2ds_status":
-                        yield from status_message_received_raw(body)
-                    elif routing_key == "os2ds_checkups":
-                        yield from checkup_message_received_raw(body)
-                    elif routing_key == "os2ds_problems":
-                        yield from problem_message_recieved_raw(body)
-            except DataError as de:
-                # DataError occurs when something went wrong trying to select
-                # or create/update data in the database. Often regarding
-                # ScheduledCheckups it is related to the json data. For now, we
-                # only log the error message.
-                logger.error(
-                        "Could not get or create object, due to DataError",
-                        error=de)
-
-
 class Command(BaseCommand):
     """Command for starting a pipeline collector process."""
     help = __doc__
@@ -327,12 +211,14 @@ class Command(BaseCommand):
                 choices=_loglevels.keys())
 
     def handle(self, *args, log, **options):
-        # change formatting to include datestamp
+        # Change formatting to include datestamp
         fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         logging.basicConfig(format=fmt, datefmt='%Y-%m-%d %H:%M:%S')
-        # set level for root logger
+        # Set level for root logger
         logging.getLogger("os2datascanner").setLevel(_loglevels[log])
 
+        # Import the CollectorRunner now we need it. Avoids circular imports.
+        from .collector_utils.collector_runner import CollectorRunner
         CollectorRunner(
-                read=["os2ds_status", "os2ds_checkups", "os2ds_problems"],
+                read=["os2ds_checkups", "os2ds_problems"],
                 prefetch_count=8).run_consumer()
