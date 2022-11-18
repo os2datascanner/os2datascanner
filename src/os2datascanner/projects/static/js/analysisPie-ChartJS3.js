8627717568eec0dbcac9d2d26f725b10d099432f
Stine <sn@magenta-aps.dk>
2022-11-18 10:39:04 +0100

    [#53497] add object storing charts for htmx, SVG icon of correct size, modal for running scan prepared

--- src/os2datascanner/projects/static/js/analysisPie-ChartJS3.js
+++ src/os2datascanner/projects/static/js/analysisPie-ChartJS3.js
@@ -1,51 +1,3 @@
-const Data = [
-    {
-        "type": "Image/png",
-        "n_files":69, 
-        "total_size":9720239, 
-        "mean":140873, 
-        "median":21267, 
-        "min":736, 
-        "max":1832836,
-    },
-    {
-        "type":"Image/jpeg",
-        "n_files":118, 
-        "total_size":45597233, 
-        "mean":386417, 
-        "median":169746, 
-        "min":2845, 
-        "max":9355552,
-    },
-    {
-        "type":"PDF",
-        "n_files":82, 
-        "total_size":10765558, 
-        "mean":131287, 
-        "median":120333, 
-        "min":5362, 
-        "max":264511,
-    },
-    {
-        "type":"XLS",
-        "n_files":105, 
-        "total_size":31064800, 
-        "mean":295855, 
-        "median":118202, 
-        "min":1213, 
-        "max":212369,
-    },
-    {
-        "type":"TXT",
-        "n_files":135, 
-        "total_size":12161999, 
-        "mean":90088, 
-        "median":92354, 
-        "min":11234, 
-        "max":150364,
-    }
-];
-
 
 function displayAsPercentage(value, ctx) {
     let dataArr = ctx.chart.data.datasets[0].data;
@@ -81,7 +33,7 @@ function formatNumber(x) {
 return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function createPie(data, htmlElements, colors){
+function createPie(data, htmlElements, colors){ // jshint ignore:line
     let [ctx, pieCanvasID] = htmlElements;
     while (colors.length < data.labels.length){
         colors = colors.concat(colors);
@@ -188,38 +140,3 @@ function createPie(data, htmlElements, colors){
     return pie;
 }
 
-function drawPies(Data, colors) {
-var ctx1 = document.getElementById("pie1");
-var ctx2 = document.getElementById("pie2");
-let types = Data.map(object => object.type);
-let nFiles = Data.map(object => object.n_files); // jshint ignore:line
-let totalSize = Data.map(object => object.total_size); // jshint ignore:line
-let pie1Data = {labels: types, data: nFiles, name: "nfiles", title: gettext("Number of files")};
-let pie2Data = {labels: types, data: totalSize, name: "storage", title: gettext("Storage space")};
-createPie(pie1Data, [ctx1, "pie1"], colors);
-createPie(pie2Data, [ctx2, "pie2"], colors);
-
-}
-
-
-
-let colorList = ["rgba(84, 71, 140)", 
-                "rgba(44, 105, 154)",
-                "rgba(4, 139, 168)",
-                "rgba(13, 179, 158)",
-                "rgba(22, 219, 147)",
-                "rgba(131, 227, 119)",
-                "rgba(185, 231, 105)",
-                "rgba(239, 234, 90)",
-                "rgba(241, 196, 83)"];
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    drawPies(Data, colorList);
-});
-    
-htmx.onLoad(function () {
-    drawPies(Data, colorList);
-});
-                  
