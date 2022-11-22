5c2bf80922146e03351dc2074497e9160b67c937
Alexander Faithfull <af@magenta.dk>
2022-11-22 09:20:40 +0100

    [#53138] Fix some odd (terminological?) errors in the heart of NameRule
    NameRule discards sensitivity values by confusing them with probabilities;
    this is now fixed. The internal helper function is_name_fragment has also
    been tweaked to make it clear that it doesn't actually do anything with
    string fragments

--- src/os2datascanner/engine2/rules/name.py
+++ src/os2datascanner/engine2/rules/name.py
@@ -82,7 +82,6 @@ class NameRule(SimpleRule):
         # Convert list of str to upper case and to sets for efficient lookup
         self.last_names = None
         self.first_names = None
-        self.all_names = None
 
         self._whitelist = frozenset(n.upper() for n in (whitelist or []))
         self._blacklist = frozenset(n.upper() for n in (blacklist or []))
@@ -92,7 +91,7 @@ class NameRule(SimpleRule):
         return "personal name"
 
     def _load_datasets(self):
-        if self.all_names is None:
+        if self.first_names is None:
             # Convert list of str to upper case and to sets for efficient
             # lookup
             m = set(map(str.upper,
@@ -108,19 +107,21 @@ class NameRule(SimpleRule):
 
             self.last_names = e
             self.first_names = f
-            self.all_names = f.union(e)
 
     def match(self, text):  # noqa: CCR001, too high cognitive complexity
         self._load_datasets()
         unmatched_text = text
 
-        def is_name_fragment(fragment, candidates, use_blacklist=True):
-            fragment = fragment.upper()
-            if use_blacklist and fragment in self._blacklist:
+        def is_name_component(
+                component: str,
+                *candidate_sets: set[str]):
+            component = component.upper()
+            if component in self._blacklist:
                 return True
+            elif component in self._whitelist:
+                return False
             else:
-                return (fragment in candidates
-                        and fragment not in self._whitelist)
+                return any(component in cs for cs in candidate_sets)
 
         # First, check for whole names, i.e. at least Firstname + Lastname
         names = match_full_name(text)
@@ -133,18 +134,20 @@ class NameRule(SimpleRule):
             # Store the original matching text
             matched_text = name[3]
 
-            first_match = is_name_fragment(first_name, self.first_names)
-            last_match = is_name_fragment(last_name, self.last_names)
+            first_match = is_name_component(first_name, self.first_names)
+            last_match = is_name_component(last_name, self.last_names)
             middle_match = any(
-                [is_name_fragment(n, self.all_names) for n in middle_names]
+                is_name_component(n, self.first_names, self.last_names)
+                for n in middle_names
             )
             # But what if the name is Word Firstname Lastname?
             while middle_match and not first_match:
                 old_name = first_name
                 first_name = middle_names.pop(0)
-                first_match = is_name_fragment(first_name, self.first_names)
+                first_match = is_name_component(first_name, self.first_names)
                 middle_match = any(
-                    [is_name_fragment(n, self.all_names) for n in middle_names]
+                    is_name_component(n, self.first_names, self.last_names)
+                    for n in middle_names
                 )
                 matched_text = matched_text.lstrip(old_name)
                 matched_text = matched_text.lstrip()
@@ -152,9 +155,10 @@ class NameRule(SimpleRule):
             while middle_match and not last_match:
                 old_name = last_name
                 last_name = middle_names.pop()
-                last_match = is_name_fragment(last_name, self.last_names)
+                last_match = is_name_component(last_name, self.last_names)
                 middle_match = any(
-                    [is_name_fragment(n, self.all_names) for n in middle_names]
+                    is_name_component(n, self.first_names, self.last_names)
+                    for n in middle_names
                 )
                 matched_text = matched_text.rstrip(old_name)
                 matched_text = matched_text.rstrip()
@@ -170,15 +174,15 @@ class NameRule(SimpleRule):
             # Check if name is blacklisted.
             # The name is blacklisted if there exists a string in the
             # blacklist which is contained as a substring of the name.
-            is_blacklisted = any([b in full_name_up for b in self._blacklist])
-            # Name match is always high sensitivity
+            is_blacklisted = any(b in full_name_up for b in self._blacklist)
+            # Name match is always high probability
             # and occurs only when first and last name are in the name lists
-            # Set sensitivity according to how many of the names were found
+            # Set probability according to how many of the names were found
             # in the names lists
             if (first_match and last_match) or is_blacklisted:
-                sensitivity = Sensitivity.CRITICAL
+                probability = 1.0
             elif first_match or last_match or middle_match:
-                sensitivity = Sensitivity.PROBLEM
+                probability = 0.5
             else:
                 continue
 
@@ -187,7 +191,10 @@ class NameRule(SimpleRule):
 
             yield {
                 "match": matched_text,
-                "sensitivity": sensitivity.value
+                "probability": probability,
+                "sensitivity": (
+                    self.sensitivity.value if self.sensitivity else None
+                ),
             }
         # Full name match done. Now check if there's any standalone names in
         # the remaining, i.e. so far unmatched string.
@@ -195,10 +202,15 @@ class NameRule(SimpleRule):
         it = name_regex.finditer(unmatched_text, overlapped=False)
         for m in it:
             matched = m.group(0)
-            if is_name_fragment(matched.upper(), self.all_names):
+            if is_name_component(
+                    matched.upper(), self.first_names, self.last_names):
                 yield {
                     "match": matched,
-                    "sensitivity": self.sensitivity
+                    "probability": 0.1,
+                    "sensitivity": (
+                        self.sensitivity.value
+                        if self.sensitivity else None
+                    ),
                 }
 
     def to_json_object(self):
