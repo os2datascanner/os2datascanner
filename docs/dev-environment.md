491c7e492ceae19076545910c5b79b6eba4dbed5
Rasmus Kristian Koefoed <rkk@magenta-aps.dk>
2023-03-14 11:18:15 +0100

    Changelog

--- docs/dev-environment.md
+++ docs/dev-environment.md
@@ -394,27 +394,55 @@ functionality. To test functionality, it is required to clone the Git-repository
     ```
     git clone https://github.com/OS2mo/os2mo.git
     cd os2mo
-    docker-compose up
     ```
-2. Go to http://localhost:5000/auth/admin/master/console/#/realms/mo/clients
-3. Enter username and password "admin"
-4. Select the client "dipex"
-5. Go to tab "Credentials"
-6. Copy the "Secret". You can regenerate a new secret here as well
-7. Open the dev-settings.toml file in the OS2datascanner-project and scroll down to "[os2mo]"  
-8. Change OS2MO_CLIENT_ID to "dipex" and OS2MO _CLIENT_SECRET to the secret
-9. Start the OS2datascanner-project. Use the "--build" flag to let the updated 
+2. Add a docker-compose.override.yml file in the root of the OS2mo project, and add the code: 
+    ```
+    services:
+      mo:
+        environment:
+          OS2MO_AUTH: "false"
+    ```
+    This allows you to access localhost:5001, using the user admin/admin. 
+    
+3. Run the container with:
+    ```
+    docker-compose up --build
+    ```
+
+4. Go to http://localhost:5000/auth/admin/master/console/#/realms/mo/clients
+5. Enter username and password "admin"
+6. Select the client "dipex"
+7. Go to tab "Credentials"
+8. Copy the "Secret". You can regenerate a new secret here as well
+9. Open the dev-settings.toml file in the OS2datascanner-project and scroll down to "[os2mo]"  
+10. Change OS2MO_CLIENT_ID to "dipex" and OS2MO _CLIENT_SECRET to the secret
+11. Start the OS2datascanner-project. Use the "--build" flag to let the updated 
 dev-settings.toml-file take effect.
     ```
     docker-compose up --build
     ```
 
-10. Go to http://localhost:8020/admin/core/client/
-11. Change your "client"-object in the Django-admin page to both allow "support of 
+12. Go to http://localhost:8020/admin/core/client/
+13. Change your "client"-object in the Django-admin page to both allow "support of 
 structured organizations" and "importservice (OS2mo)"
 
 You can now perform an OS2mo-import from the organizations-page, http://localhost:8020/organizations/.
 
+`Note:` If you would like to run a larger fixture than the default "Kolding kommune"-fixture, start by emptying the databases in both containers with the command:
+
+    docker-compose down -v --remove-orphans
+
+Repeat previous command on both containers until you no longer receive errors on either of the projects, like:
+
+    error while removing network: network os2mo_default id <id> has active endpoints
+
+When you get this warning, the database is empty:
+
+    WARNING: Network os2mo_default not found.
+
+Then build both containers again, but build the OS2mo-container with the command:
+
+    FIXTURE=aalborg docker compose up --build
 
 ## Linting and static analysis
 
--- src/os2datascanner/projects/admin/import_services/models/os2mo_import_job.py
+++ src/os2datascanner/projects/admin/import_services/models/os2mo_import_job.py
@@ -1,6 +1,5 @@
 import logging
 import requests
-import json
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -27,51 +26,74 @@ class OS2moImportJob(BackgroundJob):
             "client_secret": settings.OS2MO_CLIENT_SECRET
         }
 
-    @property
-    def job_label(self) -> str:
-        return "OS2mo Import Job"
-
-    def run(self):
+    def request_org_unit_accounts(self, org_unit_uuid: str, session: requests.Session()):
         query_org_accounts = """
-      query MyQuery {
-        org_units {
-          objects {
-            name
-            uuid
-            parent {
+        query MyQuery {
+          org_units(uuids: "%s"){
+            objects {
               name
               uuid
-            }
-            managers(inherit: true) {
-              employee {
-                givenname
-                surname
-                user_key
+              parent {
+                name
                 uuid
-                addresses(address_types: "f376deb8-4743-4ca6-a047-3241de8fe9d2") {
-                  name
+              }
+              managers(inherit: true) {
+                employee {
+                  givenname
+                  surname
+                  user_key
+                  uuid
+                  addresses(address_types: "f376deb8-4743-4ca6-a047-3241de8fe9d2") {
+                    name
+                  }
                 }
               }
-            }
-            associations {
-              employee {
-                givenname
-                surname
-                user_key
-                uuid
-                addresses(address_types: "f376deb8-4743-4ca6-a047-3241de8fe9d2") {
-                  name
+              associations {
+                employee {
+                  givenname
+                  surname
+                  user_key
+                  uuid
+                  addresses(address_types: "f376deb8-4743-4ca6-a047-3241de8fe9d2") {
+                    name
+                  }
                 }
               }
             }
           }
         }
-      }
-      """
+        """ % org_unit_uuid
         # Address_types has an email-adresses uuid, which means only employee emails will be shown
         # Org_units inherit managers from their parents
 
-        from ...organizations.os2mo_import_actions import perform_os2mo_import
+        res = session.post(url=settings.OS2MO_TOKEN_URL, data=self._make_token())
+        token = res.json().get("access_token")
+        headers = {
+            "content-type": "application/json; charset=UTF-8",
+            'Authorization': f'Bearer {token}'}
+
+        res = session.post(
+            settings.OS2MO_ENDPOINT_URL,
+            json={
+                "query": query_org_accounts},
+            headers=headers)
+        return res.json()
+
+    @property
+    def job_label(self) -> str:
+        return "OS2mo Import Job"
+
+    def run(self):
+        query_org_units = """
+        query MyQuery {
+          org_units {
+            objects {
+              uuid
+            }
+          }
+        }
+        """
+
         with requests.Session() as session:
             self.status = "Initializing OS2mo Import..."
             self.save()
@@ -85,28 +107,41 @@ class OS2moImportJob(BackgroundJob):
             res = session.post(
                 settings.OS2MO_ENDPOINT_URL,
                 json={
-                    "query": query_org_accounts},
+                    "query": query_org_units},
                 headers=headers)
-            res = json.loads(res.text)
+            res = res.json()
 
-            def _callback(action, *args):
-                self.refresh_from_db()
-                if action == "org_unit_count":
-                    count = args[0]
-                    self.to_handle = count
-                    self.handled = 0
-                    self.save()
-                elif action == "org_unit_handled":
-                    org_unit_name = args[0]
-                    self.handled += 1
-                    self.status = f"Handled {self.handled}/{self.to_handle} org_units \n" \
-                                  f"Last org_unit handled: {org_unit_name}"
-                    self.save()
+        self.status = "OK.. Data received, requesting org_units..."
+        logger.info("Received data from OS2mo. Sending queries.. \n")
 
-            self.status = "OK.. Data received, build and store relations..."
-            logger.info("Received data from OS2mo. Processing.. \n")
+        from ...organizations.os2mo_import_actions import perform_os2mo_import
 
-            perform_os2mo_import(res, self.organization, progress_callback=_callback)
+        org_unit_uuids = []
+        for org_unit_objects in res.get("data").get("org_units"):
+            for org_unit in org_unit_objects.get("objects"):
+                org_unit_uuids.append(org_unit.get("uuid"))
+
+        org_unit_list = []
+        for uuid in org_unit_uuids:
+            with requests.Session() as session:
+                logger.info(f"Querying for org_unit with uuid: {uuid}")
+                org_unit_list.append(self.request_org_unit_accounts(uuid, session))
+
+        def _callback(action, *args):
+            self.refresh_from_db()
+            if action == "org_unit_count":
+                count = args[0]
+                self.to_handle = count
+                self.handled = 0
+                self.save()
+            elif action == "org_unit_handled":
+                org_unit_name = args[0]
+                self.handled += 1
+                self.status = f"Handled {self.handled}/{self.to_handle} org_units \n" \
+                              f"Last org_unit handled: {org_unit_name}"
+                self.save()
+
+        perform_os2mo_import(org_unit_list, self.organization, progress_callback=_callback)
 
     def finish(self):
         if (pe := get_pika_thread(init=False)):
