79de358cde81ff8ac0e830d529c6e0d87f694a29
Alexander Faithfull <af@magenta.dk>
2023-05-04 18:26:05 +0200

    [#55912] Also test that hashed crunched representations stay consistent

--- src/os2datascanner/engine2/tests/test_equality.py
+++ src/os2datascanner/engine2/tests/test_equality.py
@@ -2,6 +2,8 @@ import unittest
 from parameterized import parameterized
 
 from os2datascanner.engine2.model.smbc import SMBCSource, SMBCHandle
+from os2datascanner.engine2.model.derived.pdf import (
+        PDFObjectHandle, PDFPageSource, PDFPageHandle, PDFSource)
 from os2datascanner.engine2.model.derived.mail import (
         MailSource, MailPartHandle)
 from os2datascanner.engine2.model.msgraph.mail import (
@@ -69,26 +71,28 @@ class Engine2EqualityTest(unittest.TestCase):
 
     @parameterized.expand([
         # The basics
-        (Equal1(), "Equal1(_prop=2)"),
-        (Equal1a("b"), "Equal1a(_prop=2;_other=b)"),
-        (Equal1a(None), "Equal1a(_prop=2)"),
-        (Equal3("b"), "Equal3(_prop=4)"),
+        (Equal1(), "Equal1(_prop=2)", None),
+        (Equal1a("b"), "Equal1a(_prop=2;_other=b)", None),
+        (Equal1a(None), "Equal1a(_prop=2)", None),
+        (Equal3("b"), "Equal3(_prop=4)", None),
 
         # More realistic tests
         (SMBCHandle(
                 SMBCSource("//SHARE/FILE"),
                 "path/to/document.txt"),
          "SMBCHandle(_source=(SMBCSource(_unc=//SHARE/FILE));"
-         "_relpath=path/to/document.txt)"),
+         "_relpath=path/to/document.txt)",
+         None),
         (SMBCHandle(
                 SMBCSource("//SHARE/FILE", driveletter="X"),
                 "path/to/document.txt"),
          "SMBCHandle(_source=(SMBCSource(_unc=//SHARE/FILE));"
-         "_relpath=path/to/document.txt)"),
+         "_relpath=path/to/document.txt)",
+         None),
 
         # The nightmares
         (MailPartHandle(
-                MailSource(
+                mail_source := MailSource(
                         MSGraphMailMessageHandle(
                                 MSGraphMailAccountSource(
                                         MSGraphMailAccountHandle(
@@ -108,10 +112,34 @@ class Engine2EqualityTest(unittest.TestCase):
          "(_source=(MSGraphMailAccountSource(_handle=(MSGraphMailAccountHandle"
          "(_source=(MSGraphMailSource(_tenant_id=NRTIDV));_relpath=testuser@"
          "example.invalid))));_relpath=bWVzc2FnZTI=))));"
-         "_relpath=1/Copy of Copy of FINAL (3) (EDITED) (FIXED2).doc.docx)"),
+         "_relpath=1/Copy of Copy of FINAL (3) (EDITED) (FIXED2).doc.docx)",
+         None),
+        (PDFObjectHandle(
+                PDFPageSource(
+                        PDFPageHandle(
+                                PDFSource(
+                                        MailPartHandle(
+                                                mail_source,
+                                                "2/OUTPUT.PDF",
+                                                "application/pdf")),
+                                "14")),
+                "text.html"),
+         "PDFObjectHandle(_source=(PDFPageSource(_handle=(PDFPageHandle(_sourc"
+         "e=(PDFSource(_handle=(MailPartHandle(_source=(MailSource(_handle=(MS"
+         "GraphMailMessageHandle(_source=(MSGraphMailAccountSource(_handle=(MS"
+         "GraphMailAccountHandle(_source=(MSGraphMailSource(_tenant_id=NRTIDV)"
+         ");_relpath=testuser@example.invalid))));_relpath=bWVzc2FnZTI=))));_r"
+         "elpath=2/OUTPUT.PDF))));_relpath=14))));_relpath=text.html)",
+         "87ba0c0266c59ed1f9b33d52336db3765fba80336f141249efaa2e505d924f835ead"
+         "0361f26b39bea409da6f6e673fed365ed0ce92525d61e42097d11b237001"),
     ])
-    def test_crunch(self, obj, crunch):
+    def test_crunch(self, obj, crunch, hashed):
         self.assertEqual(
                 obj.crunch(),
                 crunch,
                 "unexpected crunched representation")
+        if hashed:
+            self.assertEqual(
+                    obj.crunch(hash=True),
+                    hashed,
+                    "unexpected hashed crunched representation")
