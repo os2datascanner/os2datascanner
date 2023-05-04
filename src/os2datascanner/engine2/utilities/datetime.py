02e03a29bcd77b4245408e6064fd93b42c4cc368
Alexander Faithfull <af@magenta.dk>
2023-05-04 18:46:19 +0200

    [#34083] Make the timestamp time zone code more generically useful

--- src/os2datascanner/engine2/utilities/datetime.py
+++ src/os2datascanner/engine2/utilities/datetime.py
@@ -1,6 +1,21 @@
+from typing import Optional
 import datetime
 from dateutil.parser import parse
 from dateutil.tz import gettz
+
+
+def make_datetime_aware(
+        d: Optional[datetime.datetime],
+        local: bool = False) -> Optional[datetime.datetime]:
+    """Returns a timezone-aware timestamp, either relative to UTC (by default)
+    or to the system's local timezone. (If the input timestamp is already
+    timezone-aware, then it'll just be returned.)"""
+    return (d.replace(
+                    tzinfo=datetime.timezone.utc
+                    if not local
+                    else gettz())
+            if d and (not d.tzinfo or d.tzinfo.utcoffset(d) is None)
+            else d)
 
 
 def parse_datetime(val: str, local: bool = False) -> datetime.datetime:
@@ -10,16 +25,11 @@ def parse_datetime(val: str, local: bool = False) -> datetime.datetime:
     If the local flag is not specified or is False, then UTC will be used as
     the default fallback timezone; if it's True, then the system's local
     timezone will be used instead."""
-    rv = parse(val)
-    if not rv.tzinfo:
-        rv = rv.replace(tzinfo=datetime.timezone.utc if not local else gettz())
-    return rv
+    return make_datetime_aware(parse(val), local)
 
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 
 def unparse_datetime(d: datetime.datetime) -> str:
-    if not d.tzinfo or d.tzinfo.utcoffset(d) is None:
-        d = d.replace(tzinfo=gettz())
-    return d.strftime(DATE_FORMAT)
+    return make_datetime_aware(d).strftime(DATE_FORMAT)
