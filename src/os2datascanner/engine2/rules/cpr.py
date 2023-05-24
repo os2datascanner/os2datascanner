a8df792075f79e9e0a1d14527a46e56002bc2f53
Tomas Hagenau Andersen <tha@magenta-aps.dk>
2023-05-24 12:54:33 +0200

    [#56208] Disallow weird separators in CPR regex.

--- src/os2datascanner/engine2/rules/cpr.py
+++ src/os2datascanner/engine2/rules/cpr.py
@@ -13,7 +13,7 @@ from .utilities.cpr_probability import modulus11_check, CprProbabilityCalculator
 
 logger = structlog.get_logger(__name__)
 
-cpr_regex = r"\b(\d{2}[ ]?\d{2}[ ]?\d{2})(?:[ \-/\.\t]|[ ]\-[ ])?(\d{4})\b"
+cpr_regex = r"\b(\d{6})(?:[ \-/\.\t]|[ ]\-[ ])?(\d{4})\b"
 calculator = CprProbabilityCalculator()
 
 
