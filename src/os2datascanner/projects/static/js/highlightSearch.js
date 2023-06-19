68c2a83729c22004c6c531482af19d6cf6a3793e
Jesper <jdk@magenta-aps.dk>
2023-06-19 13:07:47 +0200

    [#56540] Fix memory leak in user error log search
    [#56540] Small fix for the highlightSearch function
    
    The htmx.onLoad-function now scopes the search to only the content being loaded in.
    
    The highlightSearch-function will no longer replace the html-elements it has itself inserted.
    
    All of the page content in error_log.html is now replaced when the form is changed to avoid weird interaction when removing focus from search bar.

--- src/os2datascanner/projects/static/js/highlightSearch.js
+++ src/os2datascanner/projects/static/js/highlightSearch.js
@@ -1,12 +1,12 @@
-function highlightText(query, className) {
+function highlightText(content, query, className) {
   // Highlights all instances of the text defined by "query" in all nodes
   // with the class defined by "className".
-  let nodes = document.querySelectorAll("td." + className);
+  let nodes = content.querySelectorAll("td." + className);
   nodes.forEach(element => {
     const queryRe = new RegExp(query.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "ig");
     let newText = element.innerHTML;
     for (const match of element.innerHTML.matchAll(queryRe)) {
-        newText = newText.replace(match[0], "<span class='text-highlight'>" + match[0] + "</span>");
+      newText = element.innerHTML.replace(match[0], "<span class='text-highlight'>" + match[0] + "</span>");
     }
 
     if (newText.length > element.innerHTML.length) {
@@ -16,8 +16,11 @@ function highlightText(query, className) {
 }
 
 
-htmx.onLoad(() => {
+htmx.onLoad(content => {
   let query = document.querySelector('#search_field').value;
-  highlightText(query, 'datatable__column--name');
-  highlightText(query, 'datatable__column--path');
+  if (query !== "") {
+    // Only do the highligh search on the content, which was just loaded in.
+    highlightText(content, query, 'datatable__column--name');
+    highlightText(content, query, 'datatable__column--path');
+  }
 });
