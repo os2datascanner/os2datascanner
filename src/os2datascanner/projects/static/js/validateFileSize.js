90c47d225718442f04a76f48ddd5011c38409e1e
Tomas Hagenau Andersen <tha@magenta-aps.dk>
2023-06-27 13:09:57 +0200

    Use modern HTML5 validation feature for file size.

--- src/os2datascanner/projects/static/js/validateFileSize.js
+++ src/os2datascanner/projects/static/js/validateFileSize.js
@@ -1,25 +1,12 @@
 // jshint unused:false
 function checkFileSize(inputElement, maxSizeStr) {
-    let runButton = document.getElementById("run-miniscan-btn");
-    let errorText = document.getElementById("file-upload-error-response").innerText;
-
-    if (inputElement.files.length !== 1) {
-        runButton.disabled = true;
-        return;
-    }
-
-    const file = inputElement.files[0];
+    const files = inputElement.files;
     const maxSize = +(maxSizeStr.replaceAll('.', ''));
+    const errorText = document.getElementById("file-upload-error-response").innerText;
 
-    if (isNaN(maxSize)) {
-        console.log("Warning: File size limit is not defined!");
-        runButton.disabled = true;
-    } else if (file.size > maxSize) {
-        console.log("This is too big");
-        runButton.disabled = true;
-        window.alert(errorText);
+    if (files.length > 0 && files[0].size > maxSize) {
+        inputElement.setCustomValidity(errorText);
     } else {
-        console.log("This is good");
-        runButton.disabled = false;
+        inputElement.setCustomValidity("");
     }
 }
