c9612d4461b0aa9e1ea8a2ba7f20a1c8bae0fc1d
Alexander Faithfull <af@magenta.dk>
2023-07-17 15:38:39 +0200

    [#54467] Test that processors don't treat transient errors as deletions
    They do, unfortunately; they didn't between versions 3.6.0 and version
    3.10.0, but then some wires got crossed

--- src/os2datascanner/engine2/tests/test_check.py
+++ src/os2datascanner/engine2/tests/test_check.py
@@ -4,7 +4,7 @@ from os2datascanner.engine2.model.core import SourceManager
 from os2datascanner.engine2.rules.cpr import CPRRule
 
 from . import model as model
-from ..pipeline import worker, messages
+from ..pipeline import worker, messages, processor
 
 
 class CheckTests(unittest.TestCase):
@@ -17,12 +17,12 @@ class CheckTests(unittest.TestCase):
 
         msg = messages.ConversionMessage(
                 scan_spec=messages.ScanSpecMessage(
-                    scan_tag=messages.ScanTagFragment.make_dummy(),
-                    source=dummy,
-                    rule=CPRRule(),
-                    configuration={},
-                    filter_rule=None,
-                    progress=None),
+                        scan_tag=messages.ScanTagFragment.make_dummy(),
+                        source=dummy,
+                        rule=CPRRule(),
+                        configuration={},
+                        filter_rule=None,
+                        progress=None),
                 handle=dummy_handle,
                 progress=messages.ProgressFragment(
                         rule=CPRRule(),
@@ -38,3 +38,72 @@ class CheckTests(unittest.TestCase):
                 sm.open(msg.scan_spec.source),
                 {dummy_handle: 1},
                 "Resource.check() called too frequently")
+
+    def test_pipeline_check_handling(self):
+        self.maxDiff = None
+
+        dummy = model.DummySource(0, secret=None)
+
+        dummy_scan_spec = messages.ScanSpecMessage(
+                scan_tag=messages.ScanTagFragment.make_dummy(),
+                source=dummy,
+                rule=CPRRule(),
+                configuration={},
+                filter_rule=None,
+                progress=None)
+
+        template = messages.ConversionMessage(
+                scan_spec=dummy_scan_spec,
+                handle=None,
+                progress=messages.ProgressFragment(
+                        rule=dummy_scan_spec.rule, matches=[]))
+
+        with SourceManager() as sm:
+            with self.subTest(
+                    "Extant objects are processed"):
+                message = template._deep_replace(
+                        handle=model.DummyHandle(
+                                dummy, 0, hints={"exists": True}))
+                replies = list(
+                        processor.message_received_raw(
+                                message.to_json_object(),
+                                "os2ds_conversions", sm))
+                self.assertCountEqual(
+                        replies,
+                        processor.message_received_raw(
+                                message.to_json_object(),
+                                "os2ds_conversions", sm),
+                        "got unexpected messages")
+
+            with self.subTest(
+                    "Deleted files are not processed"):
+                message = template._deep_replace(
+                        handle=model.DummyHandle(
+                                dummy, 0, hints={"exists": False}))
+                match list(
+                        processor.message_received_raw(
+                                message.to_json_object(),
+                                "os2ds_conversions", sm)):
+                    case [("os2ds_problems", {"missing": True}), *_]:
+                        pass
+                    case k:
+                        self.fail(
+                                'expected an "os2ds_problems" message with'
+                                ' {"missing": True}, but got' f" {k}")
+
+            with self.subTest(
+                    "Transiently unavailable files are not treated"
+                    " as though they had been deleted"):
+                message = template._deep_replace(
+                        handle=model.DummyHandle(
+                                dummy, 0, hints={"exists": None}))
+                match list(
+                        processor.message_received_raw(
+                                message.to_json_object(),
+                                "os2ds_conversions", sm)):
+                    case [("os2ds_problems", {"missing": False}), *_]:
+                        pass
+                    case k:
+                        self.fail(
+                                'expected an "os2ds_problems" message with'
+                                ' {"missing": False}, but got' f" {k}")
