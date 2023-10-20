3b14b328ff9da6978e4c5b8224a5fa2853bd7809
Alexander Faithfull <af@magenta.dk>
2023-10-20 15:57:35 +0200

    [#56036] Add some tests of the deletion scope of an import operation
    At the moment, the prepare_and_publish helper function unconditionally
    deletes all model instances not explicitly passed as arguments from all
    organisations. This is Not Good, and so these tests (reasonably enough)
    fail

--- src/os2datascanner/projects/admin/organizations/tests/test_functions.py
+++ src/os2datascanner/projects/admin/organizations/tests/test_functions.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from parameterized import parameterized
-from ..models.organization import replace_nordics
+
+from ...core.models.client import Client
+from ..models.account import Account
+from ..models.organization import Organization, replace_nordics
+
+from ..utils import prepare_and_publish
 
 
 class ReplaceSpecialCharactersTest(TestCase):
@@ -21,3 +26,72 @@ class ReplaceSpecialCharactersTest(TestCase):
         """
         replaced_string = replace_nordics(input)
         self.assertEqual(replaced_string, expected)
+
+
+class ImportHelperTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.client = Client.objects.create(
+                name="Common Client")
+        cls.org1 = Organization.objects.create(
+                client=cls.client,
+                name="Org One")
+        cls.org2 = Organization.objects.create(
+                client=cls.client,
+                name="Org Two")
+
+    def setUp(self):
+        self.mikkel = Account.objects.create(
+                username="mt@orgone.test",
+                first_name="Mikkel", last_name="Testsen",
+                organization=self.org1)
+
+    def test_prepare_and_publish_scope(self):
+        """Importing a new Account into an Organization does not delete that
+        Organization's manually-created Accounts."""
+
+        jens = Account(
+                username="jt",
+                first_name="Jens", last_name="Testsen",
+                organization=self.org1,
+
+                imported=True,
+                imported_id="jt@orgone.test")
+
+        prepare_and_publish(
+                self.org1,
+                {"jt@orgone.test"},
+                to_add=[jens],
+                to_delete=[],
+                to_update=[])
+
+        self.mikkel.refresh_from_db()
+        self.assertIsNotNone(
+                self.mikkel.pk,
+                "manually-created Account was erroneously deleted")
+
+    def test_prepare_and_publish_isolation(self):
+        """Importing a new Account into an Organization should have no effect
+        on other Organizations."""
+
+        jens = Account(
+                username="jt",
+                first_name="Jens", last_name="Testsen",
+                organization=self.org2,
+
+                imported=True,
+                imported_id="jt@orgtwo.test")
+
+        prepare_and_publish(
+                self.org2,
+                {"jt@orgtwo.test"},
+                to_add=[jens],
+                to_delete=[],
+                to_update=[])
+
+        self.mikkel.refresh_from_db()
+        self.assertIsNotNone(
+                self.mikkel.pk,
+                "manually-created Account was erroneously deleted")
