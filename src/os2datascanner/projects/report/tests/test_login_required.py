db3fc32df4a9955987e52ec8e438963bcaed3e83
Jonas Kofoed Hansen <jkh@magenta-aps.dk>
2023-10-24 16:23:27 +0200

    [#53735] exhaust generator for the tests

--- src/os2datascanner/projects/report/tests/test_login_required.py
+++ src/os2datascanner/projects/report/tests/test_login_required.py
@@ -1,15 +1,18 @@
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory, TestCase
 
+from ..organizations.models import Account, Organization
 from ..reportapp.views.report_views import UserReportView
 
 
 class TestLoginRequired(TestCase):
 
     def setUp(self):
+        org = Organization.objects.create(name="Team Saftevand")
+        self.account = Account.objects.create(
+            username='Yvonne', first_name="Yvonne",
+            last_name="Solbærsaft", organization=org)
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='Yvonne', email='yvonne@jensen.com', password='very_secret')
 
     def test_index_anonymous_user(self):
         # Tries to hit path "/" with no login
@@ -21,11 +24,11 @@ class TestLoginRequired(TestCase):
     def test_index_as_user(self):
         # Tries to hit path "/" as user
         request = self.factory.get('/')
-        user = self.user
+        user = self.account.user
         request.user = user
         response = UserReportView.as_view()(request)
 
         # Should get status code 200 OK and index.html template
         self.assertEqual(response.status_code, 200)
         self.assertIn("index.html", response.template_name)
-        user.delete()
+        self.account.delete()
